1427ce0f1b8857a34e1d42c25b38ce20b68dc3a5
Tomek Grzechowski <keo@becomewater.com>
2018-04-17 15:45:55 +0200

    event log entity missing money trait

--- app/EventLog.php
+++ app/EventLog.php
@@ -2,9 +2,56 @@
 
 namespace App;
 
+use App\ModelTraits\AccountAddress;
+use App\ModelTraits\AutomateMutators;
+use App\ModelTraits\BinHex;
+use App\ModelTraits\JsonValue;
+
 use Illuminate\Database\Eloquent\Model;
 
 class EventLog extends Model
 {
-    //
+    use AccountAddress;
+    use AutomateMutators;
+    use BinHex;
+    use JsonValue;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'cid', 'tid', 'publisher_event_id', 'banner_id', 'event_type', 'pay_to', 'ip',
+      'our_context', 'their_context', 'user_id', 'human_score', 'our_userdata', 'their_userdata',
+      'timestamp', 'event_value', 'paid_amount', 'payment_id'
+  ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+  ];
+
+    /**
+    * The attributes that use some ModelTraits with mutator settings automation
+    *
+    * @var array
+    */
+    protected $traitAutomate = [
+      'cid' => 'BinHex',
+      'tid' => 'BinHex',
+      'pay_to' => 'AccountAddress',
+      'ip' => 'BinHex',
+      'our_context' => 'JsonValue',
+      'their_context' => 'JsonValue',
+      'user_id' => 'BinHex',
+      'our_userdata' => 'JsonValue',
+      'their_userdata' => 'JsonValue',
+      // TODO: money requires some digging into money patterns
+      // 'event_value' => 'Money',
+      // 'paid_amount' => 'Money',
+  ];
 }
