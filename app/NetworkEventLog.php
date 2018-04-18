c0739a725bfedbb86f2df31e48668fcab403f1c4
Tomek Grzechowski <keo@becomewater.com>
2018-04-18 17:47:33 +0200

    net event log model update

--- app/NetworkEventLog.php
+++ app/NetworkEventLog.php
@@ -6,5 +6,52 @@ use Illuminate\Database\Eloquent\Model;
 
 class NetworkEventLog extends Model
 {
-    //
+    use AccountAddress;
+    use AutomateMutators;
+    use BinHex;
+    use JsonValue;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'cid', 'tid',
+      'banner_id',
+      'pay_from',
+      'event_type',
+      'pay_to', 'ip',
+      'context',
+      'user_id', 'human_score', 'our_userdata', 'their_userdata',
+      'timestamp',
+      'event_value', 'paid_amount', 'payment_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+    * The attributes that use some ModelTraits with mutator settings automation
+    *
+    * @var array
+    */
+    protected $traitAutomate = [
+      'cid' => 'BinHex',
+      'tid' => 'BinHex',
+      'banner_id' => 'BinHex',
+      'pay_from' => 'AccountAddress',
+      'ip' => 'BinHex',
+      'context' => 'JsonValue',
+      'user_id' => 'BinHex',
+      'our_userdata' => 'JsonValue',
+      'their_userdata' => 'JsonValue',
+      'event_value' => 'Money',
+      'paid_amount' => 'Money',
+    ];
 }
