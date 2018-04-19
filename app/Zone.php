8cf2b88fc679de732ea52447c9ca9b485c56fee5
Tomek Grzechowski <keo@becomewater.com>
2018-04-20 01:08:30 +0200

    fixing model sites, zones

--- app/Zone.php
+++ app/Zone.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Zone extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'website_id', 'name', 'width', 'height',
+    ];
 }
