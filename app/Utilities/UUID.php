76ae62ce0d476cd89a66c57ddf82a2e57caf2f41
Tomek Grzechowski <keo@becomewater.com>
2018-04-26 20:18:05 +0200

    fixing empty lines

--- app/Utilities/UUID.php
+++ app/Utilities/UUID.php
@@ -34,22 +34,17 @@ final class UUID
 
         return sprintf(
             '%08s%04s%04x%04x%12s',
-
             // 32 bits for "time_low"
             substr($hash, 0, 8),
-
             // 16 bits for "time_mid"
             substr($hash, 8, 4),
-
             // 16 bits for "time_hi_and_version",
             // four most significant bits holds version number 3
             (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
-
             // 16 bits, 8 bits for "clk_seq_hi_res",
             // 8 bits for "clk_seq_low",
             // two most significant bits holds zero and one for variant DCE1.1
             (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
-
             // 48 bits for "node"
             substr($hash, 20, 12)
         );
@@ -59,23 +54,18 @@ final class UUID
     {
         return sprintf(
             '%04x%04x%04x%04x%04x%04x%04x%04x',
-
             // 32 bits for "time_low"
             mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
-
             // 16 bits for "time_mid"
             mt_rand(0, 0xffff),
-
             // 16 bits for "time_hi_and_version",
             // four most significant bits holds version number 4
             mt_rand(0, 0x0fff) | 0x4000,
-
             // 16 bits, 8 bits for "clk_seq_hi_res",
             // 8 bits for "clk_seq_low",
             // two most significant bits holds zero and one for variant DCE1.1
             mt_rand(0, 0x3fff) | 0x8000,
-
             // 48 bits for "node"
             mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
@@ -109,22 +99,17 @@ final class UUID
 
         return sprintf(
             '%08s%04s%04x%04x%12s',
-
             // 32 bits for "time_low"
             substr($hash, 0, 8),
-
             // 16 bits for "time_mid"
             substr($hash, 8, 4),
-
             // 16 bits for "time_hi_and_version",
             // four most significant bits holds version number 5
             (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
-
             // 16 bits, 8 bits for "clk_seq_hi_res",
             // 8 bits for "clk_seq_low",
             // two most significant bits holds zero and one for variant DCE1.1
             (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
-
             // 48 bits for "node"
             substr($hash, 20, 12)
         );
