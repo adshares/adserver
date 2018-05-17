8ebd8051ac6085ba2e72d2de41cd7f1414d4d6a0
Tomek Grzechowski <keo@becomewater.com>
2018-05-17 14:19:53 +0200

    refactoringf or account address trait - statics and final (?) clean up

--- app/Models/Traits/AccountAddress.php
+++ app/Models/Traits/AccountAddress.php
@@ -7,9 +7,7 @@ namespace Adshares\Adserver\Models\Traits;
  */
 trait AccountAddress
 {
-    //  TODO: this code requires additional review in relation to half of it not being used (?)
-
-    private function crc16($hexChars)
+    private static function crc16($hexChars)
     {
         $chars = hex2bin($hexChars);
         $crc = 0x1D0F;
@@ -22,59 +20,41 @@ trait AccountAddress
         return strtoupper(str_pad(dechex($crc), 4, '0', STR_PAD_LEFT));
     }
 
-    public function encodeAddress($binAddress)
+    public static function encodeAddress($binAddress)
     {
-        $checksum = $this->crc16($binAddress);
+        $checksum = self::crc16($binAddress);
         return strtoupper(sprintf("%s-%s-%s", substr($binAddress, 0, 4), substr($binAddress, 4, 8), $checksum));
     }
 
-    // TODO: unused ? review
-    public function decodeAddress($address)
+    public static function decodeAddress($address)
     {
         $address = preg_replace('/[^0-9A-F]+/', '', strtoupper($address));
-
-        if (! preg_match('/[0-9A-F]{16}/', $address)) {
-            return null;
-        }
-
-        $checksum = substr($address, 12, 4);
-        $binAddress = substr($address, 0, 12);
-
-        if ($this->crc16($binAddress) != $checksum) {
-            return false;
-        }
-
-        return $binAddress;
-    }
-
-    public function accountAddressMutator($key, $value)
-    {
-        if ($value === null) {
-            $this->attributes[$key] = $value;
-        }
-        $address = preg_replace('/[^0-9A-F]+/', '', strtoupper($value));
 
         if (! preg_match('/[0-9A-F]{16}/', $address)) {
             throw new \InvalidArgumentException("Incorrect account address $address");
         }
         $checksum = substr($address, 12, 4);
-        $binAddress = substr($address, 0, 12);
+        return substr($address, 0, 12);
+    }
 
-//         if ($this->crc16($binAddress) != $checksum) {
-//             throw new \InvalidArgumentException("Incorrect account crc");
-//         }
-//         echo $address;exit;
+    public function accountAddressMutator($key, $value)
+    {
+        echo "(accountAddressMutator)";
+        if ($value === null) {
+            $this->attributes[$key] = $value;
+        }
+
+        $binAddress = self::decodeAddress($value);
+
         $this->attributes[$key] = hex2bin($binAddress);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function accountAddressAccessor($value)
     {
+        echo "(accountAddressAccessor)";
         if ($value === null) {
             return $value;
         }
-        return $this->encodeAddress(bin2hex($value));
+        return self::encodeAddress(bin2hex($value));
     }
 }
