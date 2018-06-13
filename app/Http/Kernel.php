04b0cc408f3c06f038c37ba351285620d45fb126
Tomek Grzechowski <keo@becomewater.com>
2018-06-13 23:43:56 +0200

    adding CORS support with configuration to allow access only to adpanel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,6 @@ class Kernel extends HttpKernel
         'guest' => \Adshares\Adserver\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }
