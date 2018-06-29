7fc64c377af2967be2c0542d947efd65a076786b
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2018-06-29 10:39:09 +0200

    Add site model

---
+++ app/Models/Site.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Adshares\Adserver\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Site extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'name', 'url',
+    ];
+}
