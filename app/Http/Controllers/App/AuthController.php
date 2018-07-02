304a957393a738fce1c60a615bf0e1cb595fa29b
Tomek Grzechowski <keo@becomewater.com>
2018-07-02 16:41:09 +0200

    logout

--- app/Http/Controllers/App/AuthController.php
+++ app/Http/Controllers/App/AuthController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends AppController
 {
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function check(Request $request)
     {
         if (Auth::check()) {
@@ -17,6 +24,13 @@ class AuthController extends AppController
         return self::json([], 401, ['message' => 'Not Authorized']);
     }
 
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request)
     {
         if (Auth::guard()->attempt(
@@ -29,5 +43,24 @@ class AuthController extends AppController
         }
 
         return self::json([], 401);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        if (!Auth::check()) {
+            return self::json([], 401, ['message' => 'Not Authorized']);
+        }
+
+        $this->guard()->logout();
+        $request->session()->invalidate();
+
+        return self::json([], 200);
     }
 }
