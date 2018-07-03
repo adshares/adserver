59b8c0e21d7f6d3c29e4aa92a482aecc5c7efd46
Tomek Grzechowski <keo@becomewater.com>
2018-07-03 13:12:25 +0200

    patching users and snaking ;-) all input for user controller

--- routes/app.php
+++ routes/app.php
@@ -8,6 +8,7 @@ Route::prefix('app')->group(function () {
     Route::get('users', 'App\UsersController@browse')->name('app.users.browse');
     Route::get('users/{user_id}', 'App\UsersController@read')->name('app.users.read');
     Route::post('users', 'App\UsersController@add')->name('app.users.add');
+    Route::patch('users/{user_id}', 'App\UsersController@edit')->name('app.users.edit');
     Route::delete('users/{user_id}', 'App\UsersController@delete')->name('app.users.delete');
     Route::post('users/email/activate', 'App\UsersController@emailActivate');
     // Sites
