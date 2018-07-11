5af37f7a8d703138cf949c037e3c8aafc8ffba54
Tomek Grzechowski <keo@becomewater.com>
2018-07-11 19:36:12 +0200

    seeding simplified and easily customizable for testing env inits

--- database/seeds/MockDataUsersSeeder.php
+++ database/seeds/MockDataUsersSeeder.php
@@ -1,51 +1,38 @@
 <?php
 
 use Adshares\Adserver\Models\User;
-
 use Illuminate\Database\Seeder;
 
 class MockDataUsersSeeder extends Seeder
 {
-    protected $password = 'test1234';
     protected $limit = 20;
+
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
     public function run()
     {
-        $this->command->info('[mock] seeding: users');
+        $this->command->info('[mock] seeding: users from users.json');
 
         if (User::count() > 0) {
-            $this->command->error('Users already seeded');
+            $this->command->error('Users table not empty - seeding only into empty db');
+
             return 999;
         }
 
-        $data = MockDataSeeder::mockDataLoad(__DIR__ . '/../mock-data/users.json');
-        $cols = array_flip($data->cols);
-        $data = $data->data;
-
-        $max = count($data) - 1;
-
-        $selected = [];
-        for ($c=0;$c<$this->limit;$c++) {
-            $selected[] = MockDataSeeder::randomNoRepeat(0, $max, $selected);
-        }
+        $data = MockDataSeeder::mockDataLoad('users.json');
 
         DB::beginTransaction();
-        foreach ($selected as $i) {
-            $r = $data[$i];
-            $u = new User;
-            $u->email = $r[$cols['email']];
-            $u->name = $r[$cols['name']];
-            $u->password = $this->password;
+        foreach ($data as $r) {
+            $u = new User();
+            $u->email = $r->email;
+            $u->password = $r->password;
             $u->save();
 
-            $this->command->info(' Added - ' . $u->email);
+            $this->command->info(" Added - <{$u->email}> with password '{$r->password}'");
         }
         DB::commit();
 
-        $this->command->info('Users mock data seeded - all passwords = test1234');
+        $this->command->info('[mock] seeding: users from users.json - DONE');
     }
 }
