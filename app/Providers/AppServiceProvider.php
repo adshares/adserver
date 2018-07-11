043ce83c527b7e2227df117be83904ad694f9a08
Tomek Grzechowski <keo@becomewater.com>
2018-07-11 20:48:57 +0200

    setting up AdsClient in place of old Esc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,42 +3,38 @@
 namespace Adshares\Adserver\Providers;
 
 use Adshares\Adserver\Services\Adselect;
-use Adshares\Esc\Esc;
-
+use Adshares\Ads\AdsClient;
+use Adshares\Ads\Driver\CliDriver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
-        //
     }
 
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
         $this->app->bind(Adselect::class, function ($app) {
             return new Adselect(config('app.adselect_endpoint'), config('app.debug'));
         });
-        $this->app->bind(Esc::class, function ($app) {
-            // Esc($walletCommand, $workingDir, $address, $secret, $host, $port)
-            return new Esc(
-                config('app.adshares_wallet'),
-                config('app.adshares_workdir'),
-                config('app.adshares_address'),
-                config('app.adshares_secret'),
-                config('app.adshares_node_host'),
-                config('app.adshares_node_port')
+        $this->app->bind(AdsClient::class, function ($app) {
+            $drv = new CliDriver(
+              config('app.adshares_address'),
+              config('app.adshares_secret'),
+              config('app.adshares_node_host'),
+              config('app.adshares_node_port')
             );
+            $drv->setCommand(config('app.adshares_command'));
+            $drv->setWorkingDir(config('app.adshares_workingdir'));
+
+            return new AdsClient($drv);
         });
     }
 }
