90de2ab029db4de246e6dbd168daa31e0fbbc5c7
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-09-07 18:35:30 +0200

    Mock missing data for panel

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,83 +26,75 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    protected $namespace = 'Adshares\Adserver\Http\Controllers';
-
     const PREFIX_AUTH = 'auth';
     const PREFIX_APP = 'panel';
     const PREFIX_API = 'api';
+    protected $namespace = 'Adshares\Adserver\Http\Controllers';
 
     public function map()
+    {
+        $this->mapWebRoutes();
+        $this->mapAuthRoutes();
+        $this->mapAppRoutes();
+        $this->mapApiRoutes();
+    }
+
+    private function mapWebRoutes(): void
     {
         Route::middleware(Kernel::WEB)
             ->get('/', function () { return view('welcome'); })
         ;
-
-        $this->mapAuthRoutes();
-
-        $this->mapApiRoutes();
-        $this->mapAppRoutes();
     }
 
     private function mapAuthRoutes(): void
     {
-        Route::middleware(Kernel::APP)
-            ->namespace($this->namespace)
-            ->prefix(self::PREFIX_AUTH)
-            ->group(
-                function () {
-                    // ApiAuthService
-                    Route::get('recovery/{token}', 'App\AuthController@recoveryTokenExtend');
-                    Route::post('recovery', 'App\AuthController@recovery');
+        Route::prefix(self::PREFIX_AUTH)->namespace($this->namespace)->group(
+            function () {
+                Route::middleware(Kernel::APP)
+                    ->group(
+                        function () {
+                            // ApiAuthService
+                            Route::get('recovery/{token}', 'App\AuthController@recoveryTokenExtend');
+                            Route::post('recovery', 'App\AuthController@recovery');
 
-                    // ApiUsersService
-                    Route::post('users', 'App\UsersController@add')->name('app.users.add');
-                    Route::patch('users', 'App\UsersController@edit');
-                }
-            )
-        ;
+                            // ApiUsersService
+                            Route::post('users', 'App\UsersController@add')->name('app.users.add');
+                            Route::patch('users', 'App\UsersController@edit');
+                        }
+                    )
+                ;
 
-        Route::middleware(Kernel::WEB)
-            ->namespace($this->namespace)
-            ->prefix(self::PREFIX_AUTH)
-            ->group(
-                function () {
-                    // ApiAuthService
-                    Route::post('login', 'App\AuthController@login');
+                Route::middleware(Kernel::WEB)
+                    ->group(
+                        function () {
+                            // ApiAuthService
+                            Route::post('login', 'App\AuthController@login');
 
-                    // ApiUsersService
-                    Route::get('users/email/confirm1Old/{token}', 'App\UsersController@emailChangeStep2');
-                    Route::get('users/email/confirm2New/{token}', 'App\UsersController@emailChangeStep3');
-                    Route::post('users/email/activate', 'App\UsersController@emailActivate');
-                }
-            )
-        ;
+                            // ApiUsersService
+                            Route::get('users/email/confirm1Old/{token}', 'App\UsersController@emailChangeStep2');
+                            Route::get('users/email/confirm2New/{token}', 'App\UsersController@emailChangeStep3');
+                            Route::post('users/email/activate', 'App\UsersController@emailActivate');
+                        }
+                    )
+                ;
 
-        Route::middleware(Kernel::API)
-            ->namespace($this->namespace)
-            ->prefix(self::PREFIX_AUTH)
-            ->group(
-                function () {
-                    // ApiAuthService
-                    Route::get('check', 'App\AuthController@check');
-                    Route::get('logout', 'App\AuthController@logout');
+                Route::middleware(Kernel::API)
+                    ->group(
+                        function () {
+                            // ApiAuthService
+                            Route::get('check', 'App\AuthController@check');
+                            Route::get('logout', 'App\AuthController@logout');
 
-                    // ApiUsersService
-                    Route::post('users/email/activate/resend', 'App\UsersController@emailActivateResend');
+                            // ApiUsersService
+                            Route::post('users/email/activate/resend', 'App\UsersController@emailActivateResend');
 
-                    Route::delete('users/{user_id}', 'App\UsersController@delete')->name('app.users.delete');
-                    Route::get('users/{user_id?}', 'App\UsersController@read')->name('app.users.read');
-                }
-            )
-        ;
-    }
-
-    private function mapApiRoutes()
-    {
-        Route::middleware(Kernel::API)
-            ->namespace($this->namespace)
-            ->prefix(self::PREFIX_API)
-            ->group(base_path('routes/api.php'))
+                            Route::delete('users/{user_id}', 'App\UsersController@delete')->name('app.users.delete');
+                            Route::get('users/{user_id?}', 'App\UsersController@read')->name('app.users.read');
+                        }
+                    )
+                ;
+            }
+        )
         ;
     }
 
@@ -112,6 +104,15 @@ class RouteServiceProvider extends ServiceProvider
             ->namespace($this->namespace)
             ->prefix(self::PREFIX_APP)
             ->group(base_path('routes/app.php'))
+        ;
+    }
+
+    private function mapApiRoutes()
+    {
+        Route::middleware(Kernel::API)
+            ->namespace($this->namespace)
+            ->prefix(self::PREFIX_API)
+            ->group(base_path('routes/api.php'))
         ;
     }
 }
