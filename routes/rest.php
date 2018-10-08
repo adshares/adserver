6eaaf5a30433bdd43abd4786cea991cbe7aefb76
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-10-08 19:22:15 +0200

    Clean routes

--- routes/rest.php
+++ routes/rest.php
@@ -18,34 +18,59 @@
  * along with AdServer. If not, see <https://www.gnu.org/licenses/>
  */
 
-Route::get('config/adshares-address', [\Adshares\Adserver\Http\Controllers\App\ConfigController::class,'adsharesAddress']);
-Route::get('notifications', [\Adshares\Adserver\Http\Controllers\App\NotificationsController::class,'read']);
-Route::get('settings/notifications', [\Adshares\Adserver\Http\Controllers\App\SettingsController::class,'readNotifications']);
+use Adshares\Adserver\Http\Controllers\App\CampaignsController;
+use Adshares\Adserver\Http\Controllers\App\ChartsController;
+use Adshares\Adserver\Http\Controllers\App\NotificationsController;
+use Adshares\Adserver\Http\Controllers\App\SitesController;
 
-Route::post('campaigns', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'add'])->name('app.campaigns.add');
-Route::get('campaigns', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'browse'])->name('app.campaigns.browse');
-Route::get('campaigns/count', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'count'])->name('app.campaigns.count');
-Route::get('campaigns/{campaign_id}', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'read'])->name('app.campaigns.read');
-Route::patch('campaigns/{campaign_id}', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'edit'])->name('app.campaigns.edit');
-Route::delete('campaigns/{campaign_id}', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'delete'])->name('app.campaigns.delete');
+Route::get('config/adshares-address', [
+    \Adshares\Adserver\Http\Controllers\App\ConfigController::class,
+    'adsharesAddress',
+]);
+Route::get('notifications', [NotificationsController::class, 'read']);
+Route::get('settings/notifications', [
+    \Adshares\Adserver\Http\Controllers\App\SettingsController::class,
+    'readNotifications',
+]);
 
-Route::post('sites', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'add'])->name('app.sites.add');
-Route::get('sites', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'browse'])->name('app.sites.browse');
-Route::get('sites/count', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'count'])->name('app.sites.count');
-Route::get('sites/{site_id}', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'read'])->name('app.sites.read');
-Route::patch('sites/{site_id}', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'edit'])->name('app.sites.edit');
-Route::delete('sites/{site_id}', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'delete'])->name('app.sites.delete');
+Route::post('campaigns', [CampaignsController::class, 'add'])
+    ->name('app.campaigns.add');
+Route::get('campaigns', [CampaignsController::class, 'browse'])
+    ->name('app.campaigns.browse');
+Route::get('campaigns/count', [CampaignsController::class, 'count'])
+    ->name('app.campaigns.count');
+Route::get('campaigns/{campaign_id}', [CampaignsController::class, 'read'])
+    ->name('app.campaigns.read');
+Route::patch('campaigns/{campaign_id}', [CampaignsController::class, 'edit'])
+    ->name('app.campaigns.edit');
+Route::delete('campaigns/{campaign_id}', [CampaignsController::class, 'delete'])
+    ->name('app.campaigns.delete');
 
-//Route::post('users', [\Adshares\Adserver\Http\Controllers\App\UsersController::class,'add'])->name('app.users.add');
-//Route::get('users', [\Adshares\Adserver\Http\Controllers\App\UsersController::class,'browse'])->name('app.users.browse');
-//Route::get('users/count', [\Adshares\Adserver\Http\Controllers\App\UsersController::class,'count'])->name('app.users.count');
-//Route::get('users/{user_id}', [\Adshares\Adserver\Http\Controllers\App\UsersController::class,'read'])->name('app.users.read');
-//Route::patch('users/{user_id}', [\Adshares\Adserver\Http\Controllers\App\UsersController::class,'edit'])->name('app.users.edit');
-//Route::delete('users/{user_id}', [\Adshares\Adserver\Http\Controllers\App\UsersController::class,'delete'])->name('app.users.delete');
+Route::post('sites', [SitesController::class, 'add'])->name('app.sites.add');
+Route::get('sites', [SitesController::class, 'browse'])
+    ->name('app.sites.browse');
+Route::get('sites/count', [SitesController::class, 'count'])
+    ->name('app.sites.count');
+Route::get('sites/{site_id}', [SitesController::class, 'read'])
+    ->name('app.sites.read');
+Route::patch('sites/{site_id}', [SitesController::class, 'edit'])
+    ->name('app.sites.edit');
+Route::delete('sites/{site_id}', [SitesController::class, 'delete'])
+    ->name('app.sites.delete');
+
+//Route::post('users', [UsersController::class, 'add'])->name('app.users.add');
+//Route::get('users', [UsersController::class, 'browse'])->name('app.users.browse');
+//Route::get('users/count', [UsersController::class, 'count'])->name('app.users.count');
+//Route::get('users/{user_id}', [UsersController::class, 'read'])->name('app.users.read');
+//Route::patch('users/{user_id}', [UsersController::class, 'edit'])->name('app.users.edit');
+//Route::delete('users/{user_id}', [UsersController::class, 'delete'])->name('app.users.delete');
 
 // tmp mocked solutions
-Route::post('chart', [\Adshares\Adserver\Http\Controllers\App\ChartsController::class,'chart']);
-Route::get('options/campaigns/targeting', [\Adshares\Adserver\Http\Controllers\App\CampaignsController::class,'targeting']);
-Route::get('options/sites/targeting', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'targeting']);
-Route::post('publisher_chart', [\Adshares\Adserver\Http\Controllers\App\ChartsController::class,'publisherChart']);
-Route::get('config/banners', [\Adshares\Adserver\Http\Controllers\App\SitesController::class,'banners']);
+Route::post('chart', [ChartsController::class, 'chart']);
+Route::get('options/campaigns/targeting', [
+    CampaignsController::class,
+    'targeting',
+]);
+Route::get('options/sites/targeting', [SitesController::class, 'targeting']);
+Route::post('publisher_chart', [ChartsController::class, 'publisherChart']);
+Route::get('config/banners', [SitesController::class, 'banners']);
--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,27 @@
  * along with AdServer. If not, see <https://www.gnu.org/licenses/>
  */
 
+use Adshares\Adserver\Http\Controllers\ApiController;
+use Adshares\Adserver\Http\Controllers\DemandController;
+use Adshares\Adserver\Http\Controllers\SupplyController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/adshares/inventory/list', [\Adshares\Adserver\Http\Controllers\ApiController::class,'adsharesInventoryList']);
-Route::get('/adshares/report/{tx_id}/{pay_to}', [\Adshares\Adserver\Http\Controllers\ApiController::class,'adsharesTransactionReport']);
+Route::get('/adshares/inventory/list', [ApiController::class, 'adsharesInventoryList']);
+Route::get('/adshares/report/{tx_id}/{pay_to}', [ApiController::class, 'adsharesTransactionReport']);
 
-Route::get('/click/{id}', [\Adshares\Adserver\Http\Controllers\DemandController::class,'click'])->name('banner-click');
-Route::get('/serve/{id}', [\Adshares\Adserver\Http\Controllers\DemandController::class,'serve'])->name('banner-serve');
-Route::get('/view/{id}', [\Adshares\Adserver\Http\Controllers\DemandController::class,'view'])->name('banner-view');
-Route::get('/view.js', [\Adshares\Adserver\Http\Controllers\DemandController::class,'viewScript'])->name('demand-view.js');
+Route::get('/click/{id}', [DemandController::class, 'click'])->name('banner-click');
+Route::get('/serve/{id}', [DemandController::class, 'serve'])->name('banner-serve');
+Route::get('/view/{id}', [DemandController::class, 'view'])->name('banner-view');
+Route::get('/view.js', [DemandController::class, 'viewScript'])->name('demand-view.js');
 
-Route::get('/l/context/{log_id}', [\Adshares\Adserver\Http\Controllers\DemandController::class,'logContext'])->name('log-context');
-Route::get('/l/keywords/{log_id}', [\Adshares\Adserver\Http\Controllers\DemandController::class,'logKeywords'])->name('log-keywords');
+Route::get('/l/context/{log_id}', [DemandController::class, 'logContext'])->name('log-context');
+Route::get('/l/keywords/{log_id}', [DemandController::class, 'logKeywords'])->name('log-keywords');
 
-Route::get('/supply/find', [\Adshares\Adserver\Http\Controllers\SupplyController::class,'find'])->name('supply-find');
-Route::get('/supply/find.js', [\Adshares\Adserver\Http\Controllers\SupplyController::class,'findScript'])->name('supply-find.js');
-Route::get('/l/n/view/{id}', [\Adshares\Adserver\Http\Controllers\SupplyController::class,'logNetworkView'])->name('log-network-view');
-Route::get('/l/n/click/{id}', [\Adshares\Adserver\Http\Controllers\SupplyController::class,'logNetworkClick'])->name('log-network-click');
-Route::get('/l/n/keywords/{log_id}', [\Adshares\Adserver\Http\Controllers\SupplyController::class,'logNetworkKeywords'])->name('log-network-keywords');
+Route::get('/supply/find', [SupplyController::class, 'find'])->name('supply-find');
+Route::get('/supply/find.js', [SupplyController::class, 'findScript'])->name('supply-find.js');
+Route::get('/l/n/view/{id}', [SupplyController::class, 'logNetworkView'])->name('log-network-view');
+Route::get('/l/n/click/{id}', [SupplyController::class, 'logNetworkClick'])->name('log-network-click');
+Route::get('/l/n/keywords/{log_id}', [SupplyController::class, 'logNetworkKeywords'])->name('log-network-keywords');
