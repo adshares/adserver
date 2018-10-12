4b055769a2f1d53ee760a0d114ee764088a6201d
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-10-12 14:19:11 +0200

    Modify paths

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use Adshares\Adserver\Http\Controllers\SupplyController;
 
 Route::get('/', function () {
     return "";
-});
+})->name('login');
 
 Route::get('/adshares/inventory/list', [ApiController::class, 'adsharesInventoryList']);
 Route::get('/adshares/report/{tx_id}/{pay_to}', [ApiController::class, 'adsharesTransactionReport']);
