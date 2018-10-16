ee4d193e6cb3817069945c65c689e2d9d7a3e409
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-10-16 16:22:00 +0200

    build and test in docker

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! -z "$TRAVIS" ]; then
+  # Checkout repo and change directory
+
+  # Install git
+  apt-get install -y git
+
+  git clone https://github.com/adshares/adserver.git /build/adserver
+  cd /build/adserver
+fi
+
+envsubst < .env.dist | tee .env
+
+composer install
+
+artisan key:generate
+artisan package:discover
+
+yarn install
+yarn run dev
