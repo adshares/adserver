54f57ec7827279b8d22d30fa8b0082e4b116bcd3
PawelPodkalicki <ppodkali@gmail.com>
2018-10-17 13:39:04 +0200

    Add console commands to scan ADS chain

---
+++ app/Console/Commands/AdsProcessTx.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Copyright (c) 2018 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Adserver\Console\Commands;
+
+use Adshares\Ads\AdsClient;
+use Adshares\Ads\Driver\CommandError;
+use Adshares\Ads\Entity\Transaction\SendManyTransaction;
+use Adshares\Ads\Entity\Transaction\SendManyTransactionWire;
+use Adshares\Ads\Entity\Transaction\SendOneTransaction;
+use Adshares\Ads\Exception\CommandException;
+use Adshares\Adserver\Facades\DB;
+use Adshares\Adserver\Models\AdsTxIn;
+use Adshares\Adserver\Models\User;
+use Adshares\Adserver\Models\UserLedger;
+use Illuminate\Console\Command;
+
+class AdsProcessTx extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ads:process-tx';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Processes incoming txs';
+
+    /**
+     * Command ended without error
+     */
+    const EXIT_CODE_SUCCESS = 0;
+
+    /**
+     * Command ended prematurely because block ids could not be updated
+     */
+    const EXIT_CODE_CANNOT_GET_BLOCK_IDS = 1;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * @param AdsClient $adsClient
+     * @return int
+     */
+    public function handle(AdsClient $adsClient)
+    {
+        try {
+            $this->updateBlockIds($adsClient);
+        } catch (CommandException $exc) {
+            $code = $exc->getCode();
+            $message = $exc->getMessage();
+            $this->error("Cannot update blocks due to CommandException:\n"
+                . "Code:\n  ${code}\n"
+                . "Message:\n  ${message}\n");
+            return self::EXIT_CODE_CANNOT_GET_BLOCK_IDS;
+        }
+
+        $adServerAddress = config('app.adshares_address');
+        $txs = AdsTxIn::where('status', AdsTxIn::STATUS_NEW)->get();
+        /** @var $tx AdsTxIn */
+        foreach ($txs as $tx) {
+            $transaction = $adsClient->getTransaction($tx->txid)->getTxn();
+            $type = $transaction->getType();
+            switch ($type) {
+                case 'send_many':
+                    $isTxTargetValid = false;
+                    /** @var $sendManyTx SendManyTransaction */
+                    $sendManyTx = $transaction;
+//                    $senderAddr = $sendManyTx->getSenderAddress();
+                    $wiresCount = $sendManyTx->getWireCount();
+                    if ($wiresCount > 0) {
+                        $wires = $sendManyTx->getWires();
+                        foreach ($wires as $wire) {
+                            /** @var $wire SendManyTransactionWire */
+                            $targetAddr = $wire->getTargetAddress();
+                            if ($targetAddr === $adServerAddress) {
+//                                $amount = $wire->getAmount();
+                                $isTxTargetValid = true;
+                                break;
+                            }
+                        }
+                    }
+                    $tx->status = $isTxTargetValid ? AdsTxIn::STATUS_RESERVED : AdsTxIn::STATUS_INVALID;
+                    $tx->save();
+                    break;
+
+                case 'send_one':
+                    /** @var $sendOneTx SendOneTransaction */
+                    $sendOneTx = $transaction;
+                    $targetAddr = $sendOneTx->getTargetAddress();
+                    if ($targetAddr === $adServerAddress) {
+                        $message = $sendOneTx->getMessage();
+                        $user = User::where('uuid', hex2bin($this->extractUuidFromMessage($message)))->first();
+                        if (null === $user) {
+                            $tx->status = AdsTxIn::STATUS_RESERVED;
+                            $tx->save();
+                        } else {
+//                            $senderAddr = $sendOneTx->getSenderAddress();
+                            $amount = $sendOneTx->getAmount();
+                            // add to ledger
+                            $ul = new UserLedger;
+                            $ul->users_id = $user->id;
+                            $ul->amount = $amount;
+                            $ul->desc = $tx->txid;
+
+                            $tx->status = AdsTxIn::STATUS_USER_DEPOSIT;
+                            // tx added to ledger will not be processed again
+                            DB::transaction(function () use ($ul, $tx) {
+                                $ul->save();
+                                $tx->save();
+                            });
+                        }
+                        break;
+                    } else {
+                        $tx->status = AdsTxIn::STATUS_INVALID;
+                        $tx->save();
+                    }
+                    break;
+
+                default:
+                    $tx->status = AdsTxIn::STATUS_INVALID;
+                    $tx->save();
+                    break;
+            }
+        }
+        return self::EXIT_CODE_SUCCESS;
+    }
+
+    /**
+     * Extracts uuid from tx message.
+     * @param string $message tx message
+     * @return string uuid as hex string
+     */
+    private function extractUuidFromMessage(string $message): string
+    {
+        return substr($message, -32);
+    }
+
+    /**
+     * Updates block data.
+     * @param AdsClient $adsClient
+     * @throws CommandException
+     */
+    private function updateBlockIds(AdsClient $adsClient)
+    {
+        $attempt = 0;
+        $attemptMax = 5;
+        while (true) {
+            try {
+                $response = $adsClient->getBlockIds();
+                $updatedBlocks = $response->getUpdatedBlocks();
+                $this->info("Updated blocks: ${updatedBlocks}");
+                if ($updatedBlocks === 0) {
+                    break;
+                }
+            } catch (CommandException $exc) {
+                if (CommandError::GET_SIGNATURE_UNAVAILABLE === $exc->getCode()
+                    && ++$attempt < $attemptMax) {
+                    // try again after 3 seconds sleep
+                    sleep(3);
+                } else {
+                    throw $exc;
+                }
+            }
+        }
+    }
+}
---
+++ tests/Console/Commands/AdsProcessTxTest.php
@@ -0,0 +1,310 @@
+<?php
+/**
+ * Copyright (c) 2018 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Adserver\Tests\Console\Commands;
+
+use Adshares\Ads\AdsClient;
+use Adshares\Ads\Command\GetBlockIdsCommand;
+use Adshares\Ads\Exception\CommandException;
+use Adshares\Ads\Response\GetBlockIdsResponse;
+use Adshares\Ads\Response\GetTransactionResponse;
+use Adshares\Adserver\Console\Commands\AdsProcessTx;
+use Adshares\Adserver\Models\AdsTxIn;
+use Adshares\Adserver\Models\User;
+use Adshares\Adserver\Models\UserLedger;
+use Adshares\Adserver\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AdsProcessTxTest extends TestCase
+{
+    use RefreshDatabase;
+
+    const TX_ID_SEND_ONE = '0001:00000083:0001';
+    const TX_ID_SEND_MANY = '0001:00000085:0001';
+
+    public function testAdsProcessValidUserDeposit()
+    {
+        $depositAmount = 100000000000;
+
+        $adsTx = new AdsTxIn;
+        $adsTx->txid = self::TX_ID_SEND_ONE;
+        $adsTx->amount = $depositAmount;
+        $adsTx->address = '0001-00000000-9B6F';
+        $adsTx->save();
+
+        $user = factory(User::class)->create();
+        $user->uuid = '00000000000000000000000000000123';
+        $user->save();
+
+        $this->assertEquals(AdsTxIn::STATUS_NEW, AdsTxIn::all()->first()->status);
+
+        $this->artisan('ads:process-tx')
+            ->assertExitCode(AdsProcessTx::EXIT_CODE_SUCCESS);
+
+        $this->assertEquals(AdsTxIn::STATUS_USER_DEPOSIT, AdsTxIn::all()->first()->status);
+        $amount = UserLedger::where('users_id', $user->id)->sum('amount');
+        $this->assertEquals($depositAmount, $amount);
+    }
+
+    public function testAdsProcessValidSendMany()
+    {
+        $adsTx = new AdsTxIn;
+        $adsTx->txid = self::TX_ID_SEND_MANY;
+        $adsTx->amount = 300000000000;
+        $adsTx->address = '0001-00000000-9B6F';
+        $adsTx->save();
+
+        $this->assertEquals(AdsTxIn::STATUS_NEW, AdsTxIn::all()->first()->status);
+
+        $this->artisan('ads:process-tx')
+            ->assertExitCode(AdsProcessTx::EXIT_CODE_SUCCESS);
+
+        $this->assertEquals(AdsTxIn::STATUS_RESERVED, AdsTxIn::all()->first()->status);
+    }
+
+    public function testAdsProcessGetBlockIdsError()
+    {
+        $this->app->bind(AdsClient::class, function () {
+            $adsClient = $this->createMock(AdsClient::class);
+            // getBlockIds
+            $command = new GetBlockIdsCommand('0', '5B400000');
+            $exception = new CommandException($command, 'Process timed out');
+            $adsClient->method('getBlockIds')->willThrowException($exception);
+            return $adsClient;
+        });
+
+        $adsTx = new AdsTxIn;
+        $adsTx->txid = self::TX_ID_SEND_ONE;
+        $adsTx->amount = 100000000000;
+        $adsTx->address = '0001-00000000-9B6F';
+        $adsTx->save();
+
+        $this->assertEquals(AdsTxIn::STATUS_NEW, AdsTxIn::all()->first()->status);
+
+        $this->artisan('ads:process-tx')
+            ->assertExitCode(AdsProcessTx::EXIT_CODE_CANNOT_GET_BLOCK_IDS);
+
+        $this->assertEquals(AdsTxIn::STATUS_NEW, AdsTxIn::all()->first()->status);
+    }
+
+    private function getBlockIds1(): GetBlockIdsResponse
+    {
+        return new GetBlockIdsResponse(json_decode('{
+            "current_block_time": "1539704320",
+            "previous_block_time": "1539703808",
+            "tx": {
+                "data": "13010005000000AB07C65B0000C65B00000000",
+                "signature": "165A66C3F7B81FC8E0A3684C57EF9F3DDDD64C4A52E64FF953F4A7C9CF7348882'
+            . 'E4FB214EF1393BA94BDCC071A15B3C2C5400A9430CFA2CC8AEFA3CAE645E708",
+                "time": "1539704747"
+            },
+            "updated_blocks": "3",
+            "blocks": [
+                "5BC60000",
+                "5BC60200",
+                "5BC60400"
+            ]
+        }', true));
+    }
+
+    private function getBlockIds2(): GetBlockIdsResponse
+    {
+        return new GetBlockIdsResponse(json_decode('{
+            "current_block_time": "1539704320",
+            "previous_block_time": "1539703808",
+            "tx": {
+                "data": "13010005000000AD07C65B0006C65B00000000",
+                "signature": "D709A9B39D8002A3DCE42563EF2D5234B3676AA4997D736621F8A99F53A7'
+            . 'A35A7D971B33E76A9E7232496ED68765CC39E8B9362F220CA4849CCFE4A08D24F80B",
+                "time": "1539704749"
+            },
+            "updated_blocks": "0"
+        }', true));
+    }
+
+    private function getTxSendOne(): string
+    {
+        return '{
+            "current_block_time": "1539179872",
+            "previous_block_time": "1539179840",
+            "tx": {
+                "data": "140100000000007A05BE5B0100830000000100",
+                "signature": "4A6BBAEB8BCEF9702FF5AADEFE2CD3CF74F2F7D5F555F7F2D7E4E96F4CF4511E7B'
+            . '894289452AC50AA63964638D4E2EB428ED910309DDC477519CFE428B067F06",
+                "time": "1539179898",
+                "account_msid": "0",
+                "account_hashin": "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                "account_hashout": "1A2CDFA26C7FDD12BD98D1009F34A5B88CDCB2F7DC7E6282BEEDD13B8A0B4684",
+                "deduct": "0.00000000000",
+                "fee": "0.00000000000"
+            },
+            "network_tx": {
+                "id": "0001:00000083:0001",
+                "block_time": "1539178368",
+                "block_id": "5BBDFF80",
+                "node": "1",
+                "node_msid": "131",
+                "node_mpos": "1",
+                "size": "125",
+                "hashpath_size": "7",
+                "data": "04010000000000010000008EFFBD5B01000000000000E876481700000000000000000000'
+            . '0000000000000000000000000000000000000000000000000086A3F063531BAC0F779F6172FAEDF09A6'
+            . '606D64FBEC69E4B6ED779D171FEA7C94E1D430F1782E4621F10D758E1A4D6039162B8303AD359A38A2590D05E0AF909",
+                "hashpath": [
+                    "4F6441EDA96E38F5994C09F497D1BB15C3CF860163A4BA3703190BF1CF602E42",
+                    "7A6068E0B7A098FE0994C9427D8A39A0B7462951EAA80DA0310F8A0B0A2C7E61",
+                    "D20418CD011D63997A59391AB9BF1DE35C624C1B6000C21338305BEB378594E9",
+                    "1904710E9FD4BA678B28BA5ACBFEB17C43020EC27A5A842771F76738A132CEB6",
+                    "F1221A7CAD4E5873EA13449D072DF6B99E7EEC005CCE76A44A12D548FF0583B3",
+                    "B6E8891D131FCD325D93C4D587A0DA6F0E8C8F0602638235A750FD83D6D8101C",
+                    "DB00586D8E108F14BD53328B63DFE1E8B94E7A9728FDE6D20545966C57934AF8"
+                ]
+            },
+            "txn": {
+                "type": "send_one",
+                "node": "1",
+                "user": "0",
+                "msg_id": "1",
+                "time": "1539178382",
+                "target_node": "1",
+                "target_user": "5",
+                "sender_fee": "0.00050000000",
+                "sender_address": "0001-00000000-9B6F",
+                "target_address": "0001-00000005-CBCA",
+                "amount": "1.00000000000",
+                "message": "0000000000000000000000000000000000000000000000000000000000000123",
+                "signature": "86A3F063531BAC0F779F6172FAEDF09A6606D64FBEC69E4B6ED779D17'
+            . '1FEA7C94E1D430F1782E4621F10D758E1A4D6039162B8303AD359A38A2590D05E0AF909"
+            }
+        }';
+    }
+
+    private function getTxSendMany(): string
+    {
+        return '{
+            "current_block_time": "1539179488",
+            "previous_block_time": "1539179456",
+            "tx": {
+                "data": "14010000000000F403BE5B0100850000000100",
+                "signature": "F501E9507021B49423DE3CBF4BBA7829145D2595873F75070B8A5F1A1D0E71E7BC'
+            . '999CA737DA2E5A4B2DFC296D2F441078D47081D10A2C9249BDFE278DB1120F",
+                "time": "1539179508",
+                "account_msid": "0",
+                "account_hashin": "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                "account_hashout": "A88AB7CA15CFB52E3EB6C4E4DC0BA1FC09A7FBC4920553CF605D37E6185BEF91",
+                "deduct": "0.00000000000",
+                "fee": "0.00000000000"
+            },
+            "network_tx": {
+                "id": "0001:00000085:0001",
+                "block_time": "1539178432",
+                "block_id": "5BBDFFC0",
+                "node": "1",
+                "node_msid": "133",
+                "node_mpos": "1",
+                "size": "95",
+                "hashpath_size": "7",
+                "data": "0501000000000003000000CDFFBD5B010001000000000000B864D94500000084966'
+            . '1171A830B4412AE89A8B0C3A29E05D1AE2D521232529F2CCFD1626B8681D385884C3193'
+            . 'D11D3993CD3F9AD90E210D362FEDBE5DCDC092DE8DDBAC4FDA07",
+                "hashpath": [
+                    "9638C9D17A7BDB5309C46D6B78365853230459AE606BA3181C94456022523F4C",
+                    "1737AEDFD0F8EEB8E39BC3DF08C4FEE79197D0CAB7CAB682FE4F26E272DA380F",
+                    "D20418CD071D63997A59391AB9BF1DE35C624C1B6000C21338305BEB378594E9",
+                    "7670510D4E7D8AB620C07D2569115C76DD626DC073ECC203663FF0DB94B954AF",
+                    "8EE49D07979D7891399F2E3B405E4E36CCCE2AE7E1EB88916A03C8EFB8E8F680",
+                    "AF4FD5922526C6C8FA2506C45315B25E4A961099E7688A7C044630E44D056956",
+                    "954AEDF00ABF157D1E01E678D84F8D903A392A9844C5C25D6A05726C9BCBEB5A"
+                ]
+            },
+            "txn": {
+                "type": "send_many",
+                "node": "1",
+                "user": "0",
+                "sender_address": "0001-00000000-9B6F",
+                "msg_id": "3",
+                "time": "1539178445",
+                "wire_count": "1",
+                "sender_fee": "0.00150000000",
+                "wires": [
+                    {
+                        "target_node": "1",
+                        "target_user": "0",
+                        "target_address": "0001-00000005-CBCA",
+                        "amount": "3.00000000000"
+                    }
+                ],
+                "signature": "849661171A830B4412AE89A8B0C3A29E05D1AE2D521232529F2CCFD1'
+            . '626B8681D385884C3193D11D3993CD3F9AD90E210D362FEDBE5DCDC092DE8DDBAC4FDA07"
+            }
+        }';
+    }
+
+    /**
+     * @param string $txid
+     * @return GetTransactionResponse
+     * @throws \Exception
+     */
+    private function getTx(string $txid): GetTransactionResponse
+    {
+        switch ($txid) {
+            case self::TX_ID_SEND_ONE:
+                $response = $this->getTxSendOne();
+                break;
+            case self::TX_ID_SEND_MANY:
+                $response = $this->getTxSendMany();
+                break;
+
+            default:
+                throw new \Exception();
+        }
+
+        return new GetTransactionResponse(json_decode($response, true));
+    }
+
+    private function mockAdsClient(): void
+    {
+        $this->app->bind(AdsClient::class, function () {
+            $adsClient = $this->createMock(AdsClient::class);
+            // getBlockIds
+            $getBlockIdsStub = new \PHPUnit\Framework\MockObject\Stub\ConsecutiveCalls([
+                $this->getBlockIds1(),
+                $this->getBlockIds2(),
+            ]);
+            $adsClient->method('getBlockIds')->will($getBlockIdsStub);
+
+            // getTransaction
+            $txIds = [self::TX_ID_SEND_ONE, self::TX_ID_SEND_MANY];
+            $map = [];
+            foreach ($txIds as $txId) {
+                array_push($map, [$txId, $this->getTx($txId)]);
+            }
+            $adsClient->method('getTransaction')
+                ->will($this->returnValueMap($map));
+            return $adsClient;
+        });
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockAdsClient();
+    }
+}
