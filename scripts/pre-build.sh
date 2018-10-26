a2bcaf40072caaa7679c91ca871c5428b34d01c5
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-10-26 11:26:23 +0200

    mysql

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -5,6 +5,8 @@
 # Install dependencies for composer operations
 apt-get -qq -y install php7.2-cli php7.2-curl php7.2-zip php7.2-xdebug php7.2-mysql php7.2-gd unzip
 
+phpenmod pdo_mysql
+
 # Get composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-mysql --version || apt-get -qq -y install mysql-server
-
 apt-get -qq -y install php7.2-cli php7.2-fpm php7.2-curl php7.2-mysql
