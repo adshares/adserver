c4348398b934e4414f561ecd1ac5fc2f8f2d300f
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-11-02 18:28:17 +0100

    CP from PAN-63

--- app/Http/Controllers/Rest/SitesController.php
+++ app/Http/Controllers/Rest/SitesController.php
@@ -35,8 +35,6 @@ class SitesController extends Controller
         $site->user_id = Auth::user()->id;
         $site->save();
 
-        $site->zones()->createMany($request->input('site.ad_units'));
-
         return self::json([], Response::HTTP_CREATED)
             ->header('Location', route('app.sites.read', ['site' => $site]));
     }
@@ -53,11 +51,12 @@ class SitesController extends Controller
 
     public function browse()
     {
-        return self::json(Site::get());
+        return self::json(Site::get()->toArray());
     }
 
-    public function count()
+    public function count(Request $request)
     {
+        //@TODO: create function data
         $siteCount = [
             'totalEarnings' => 0,
             'totalClicks' => 0,
@@ -73,11 +72,11 @@ class SitesController extends Controller
     {
         $site->delete();
 
-        return self::json(['message' => 'Successfully deleted']);
+        return self::json(['message' => 'Successfully deleted'], Response::HTTP_NO_CONTENT);
     }
 
     public function read(Site $site)
     {
-        return self::json($site);
+        return self::json($site->toArray());
     }
 }
