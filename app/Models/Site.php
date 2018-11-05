99ce9ef04305cd5b0ae6a50796f3db2f306ffd82
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-11-05 19:30:49 +0100

    Propagate site status to zone

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -23,13 +23,14 @@ namespace Adshares\Adserver\Models;
 use Adshares\Adserver\Models\Traits\Ownership;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Collection;
 
 /**
  * @property int user_id
  * @property string name
  * @property array|null|string site_requires
  * @property array|null|string site_excludes
- * @property Zone[] zones
+ * @property Zone[]|Collection zones
  * @method static Site create($input = null)
  * @method static get()
  */
@@ -37,6 +38,15 @@ class Site extends Model
 {
     use Ownership;
     use SoftDeletes;
+    public const STATUS_DRAFT = 0;
+    public const STATUS_INACTIVE = 1;
+    public const STATUS_ACTIVE = 2;
+    public const STATUSES = [self::STATUS_DRAFT, self::STATUS_INACTIVE, self::STATUS_ACTIVE];
+    private const ZONE_STATUS = [
+        Site::STATUS_DRAFT => Zone::STATUS_DRAFT,
+        Site::STATUS_INACTIVE => Zone::STATUS_ARCHIVED,
+        Site::STATUS_ACTIVE => Zone::STATUS_ACTIVE,
+    ];
     public static $rules = [
         'name' => 'required|max:64',
         'primary_language' => 'required|max:2',
@@ -97,5 +107,13 @@ class Site extends Model
         $serverUrl = config('app.url');
 
         return "<script src=\"$serverUrl/supply/find.js\" async></script>";
+    }
+
+    public function setStatusAttribute($value)
+    {
+        $this->attributes['status'] = $value;
+        $this->zones->map(function (Zone $zone) use ($value) {
+            $zone->status = Site::ZONE_STATUS[$value];
+        });
     }
 }
