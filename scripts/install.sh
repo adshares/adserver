6e0f4b344845836a377241a4bc1db97845b1cea9
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-07 13:14:13 +0100

    mysql and installation path

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,27 +3,27 @@
 set -e
 
 # Create directories
-mkdir -p ${ADSERVER_INSTALLATION_DIR}
-mkdir -pm 777 ${ADSERVER_INSTALLATION_DIR}/storage
+mkdir -p ${INSTALLATION_PATH}
+mkdir -pm 777 ${INSTALLATION_PATH}/storage
 
 # Move directories
-mv app ${ADSERVER_INSTALLATION_DIR}/
-mv bin ${ADSERVER_INSTALLATION_DIR}/
-mv bootstrap ${ADSERVER_INSTALLATION_DIR}/
-mv config ${ADSERVER_INSTALLATION_DIR}/
-mv database ${ADSERVER_INSTALLATION_DIR}/
-mv node_modules ${ADSERVER_INSTALLATION_DIR}/
-mv public ${ADSERVER_INSTALLATION_DIR}/
-mv resources ${ADSERVER_INSTALLATION_DIR}/
-mv routes ${ADSERVER_INSTALLATION_DIR}/
-mv tests ${ADSERVER_INSTALLATION_DIR}/
-mv vendor ${ADSERVER_INSTALLATION_DIR}/
+mv app ${INSTALLATION_PATH}/
+mv bin ${INSTALLATION_PATH}/
+mv bootstrap ${INSTALLATION_PATH}/
+mv config ${INSTALLATION_PATH}/
+mv database ${INSTALLATION_PATH}/
+mv node_modules ${INSTALLATION_PATH}/
+mv public ${INSTALLATION_PATH}/
+mv resources ${INSTALLATION_PATH}/
+mv routes ${INSTALLATION_PATH}/
+mv tests ${INSTALLATION_PATH}/
+mv vendor ${INSTALLATION_PATH}/
 
 # Move artisan binary
-mv artisan ${ADSERVER_INSTALLATION_DIR}/
+mv artisan ${INSTALLATION_PATH}/
 
-mv composer.json ${ADSERVER_INSTALLATION_DIR}/
+mv composer.json ${INSTALLATION_PATH}/
 
 # Migrate
-cd ${ADSERVER_INSTALLATION_DIR}
+cd ${INSTALLATION_PATH}
 ./artisan migrate
