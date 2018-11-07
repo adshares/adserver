6e2d867334e3c1289154f316a749048acc274bff
awlodarkiewicz <37933734+awlodarkiewicz@users.noreply.github.com>
2018-11-07 17:13:49 +0100

    Travis docker (#151)
    New Travis and build configuration

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Create directories
+mkdir -p ${INSTALLATION_PATH}
+mkdir -pm 777 ${INSTALLATION_PATH}/storage
+
+# Move directories
+mv app ${INSTALLATION_PATH}/
+mv bin ${INSTALLATION_PATH}/
+mv bootstrap ${INSTALLATION_PATH}/
+mv config ${INSTALLATION_PATH}/
+mv database ${INSTALLATION_PATH}/
+mv node_modules ${INSTALLATION_PATH}/
+mv public ${INSTALLATION_PATH}/
+mv resources ${INSTALLATION_PATH}/
+mv routes ${INSTALLATION_PATH}/
+mv tests ${INSTALLATION_PATH}/
+mv vendor ${INSTALLATION_PATH}/
+
+# Move artisan binary
+mv artisan ${INSTALLATION_PATH}/
+
+mv composer.json ${INSTALLATION_PATH}/
+
+# Migrate
+cd ${INSTALLATION_PATH}
+./artisan migrate
