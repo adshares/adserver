16dcacbda0bf7b6cf7f84460559a74e8016355b4
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-07 17:53:58 +0100

    don't forget fpm

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+apt-get -qq -y install php7.2-fpm
+
 apt-get -qq -y install \
         php7.2-cli php7.2-mysql php7.2-bcmath php7.2-bz2 php7.2-cli php7.2-curl \
         php7.2-gd php7.2-intl php7.2-json php7.2-mbstring php7.2-opcache \
