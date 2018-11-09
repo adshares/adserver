08c8b8659faaadd220e810bbe6adcf45e7741f04
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-11-09 15:43:15 +0100

    Reformatted PHP (2)

--- tests/Http/Rest/CampaignsTest.php
+++ tests/Http/Rest/CampaignsTest.php
@@ -46,7 +46,7 @@ final class CampaignsTest extends TestCase
     {
         $this->actingAs(factory(User::class)->create(), 'api');
 
-        $response = $this->getJson(self::URI . '/1');
+        $response = $this->getJson(self::URI.'/1');
         $response->assertStatus(Response::HTTP_NOT_FOUND);
     }
 
@@ -59,59 +59,8 @@ final class CampaignsTest extends TestCase
 
         $id = $this->getIdFromLocation($response->headers->get('Location'));
 
-        $response = $this->getJson(self::URI . '/' . $id);
+        $response = $this->getJson(self::URI.'/'.$id);
         $response->assertStatus(Response::HTTP_OK);
-    }
-
-    public function testDeleteCampaignWithBanner(): void
-    {
-        $user = factory(User::class)->create();
-        $this->actingAs($user, 'api');
-
-        $campaignId = $this->createCampaignForUser($user);
-        $bannerId = $this->createBannerForCampaign($campaignId);
-
-        $this->assertCount(1, Campaign::where('id', $campaignId)->get());
-        $this->assertCount(1, Banner::where('id', $bannerId)->get());
-
-        $response = $this->deleteJson(self::URI . "/{$campaignId}");
-        $response->assertStatus(Response::HTTP_NO_CONTENT);
-
-        $this->assertCount(0, Campaign::where('id', $campaignId)->get());
-        $this->assertCount(0, Banner::where('id', $bannerId)->get());
-        $this->assertCount(1, Campaign::withTrashed()->where('id', $campaignId)->get());
-        $this->assertCount(1, Banner::withTrashed()->where('id', $bannerId)->get());
-
-        $response = $this->deleteJson(self::URI . "/{$campaignId}");
-        $response->assertStatus(Response::HTTP_NOT_FOUND);
-    }
-
-    public function testFailDeleteNotOwnedCampaign(): void
-    {
-        $this->actingAs(factory(User::class)->create(), 'api');
-
-        $user = factory(User::class)->create();
-        $campaignId = $this->createCampaignForUser($user);
-        $this->createBannerForCampaign($campaignId);
-
-        $response = $this->deleteJson(self::URI . "/{$campaignId}");
-        $response->assertStatus(Response::HTTP_NOT_FOUND);
-    }
-
-    private function createCampaignForUser(User $user): int
-    {
-        $campaign = factory(Campaign::class)->create(['user_id' => $user->id]);
-        $campaignId = $campaign->id;
-
-        return $campaignId;
-    }
-
-    private function createBannerForCampaign(int $campaignId): int
-    {
-        $banner = factory(Banner::class)->create(['campaign_id' => $campaignId]);
-        $bannerId = $banner->id;
-
-        return $bannerId;
     }
 
     private function campaignInputData(): array
@@ -119,13 +68,13 @@ final class CampaignsTest extends TestCase
         return [
             'basicInformation' => [
                 'status' => 0,
-			    'name' =>  'Adshares test campaign',
-			    'targetUrl' => 'http://adshares.net',
-			    'max_cpc' => 2,
-			    'max_cpm' => 1,
-			    'budget' => 100,
-			    'dateStart' => '2018-01-01',
-			    'dateEnd' => '2018-12-30',
+                'name' => 'Adshares test campaign',
+                'targetUrl' => 'http://adshares.net',
+                'max_cpc' => 2,
+                'max_cpm' => 1,
+                'budget' => 100,
+                'dateStart' => '2018-01-01',
+                'dateEnd' => '2018-12-30',
             ],
             'targeting' => [
                 'requires' => [],
@@ -145,5 +94,56 @@ final class CampaignsTest extends TestCase
         $this->assertSame(1, preg_match('/(\d+)$/', $location, $matches));
 
         return $matches[1];
+    }
+
+    public function testDeleteCampaignWithBanner(): void
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user, 'api');
+
+        $campaignId = $this->createCampaignForUser($user);
+        $bannerId = $this->createBannerForCampaign($campaignId);
+
+        $this->assertCount(1, Campaign::where('id', $campaignId)->get());
+        $this->assertCount(1, Banner::where('id', $bannerId)->get());
+
+        $response = $this->deleteJson(self::URI."/{$campaignId}");
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $this->assertCount(0, Campaign::where('id', $campaignId)->get());
+        $this->assertCount(0, Banner::where('id', $bannerId)->get());
+        $this->assertCount(1, Campaign::withTrashed()->where('id', $campaignId)->get());
+        $this->assertCount(1, Banner::withTrashed()->where('id', $bannerId)->get());
+
+        $response = $this->deleteJson(self::URI."/{$campaignId}");
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    private function createCampaignForUser(User $user): int
+    {
+        $campaign = factory(Campaign::class)->create(['user_id' => $user->id]);
+        $campaignId = $campaign->id;
+
+        return $campaignId;
+    }
+
+    private function createBannerForCampaign(int $campaignId): int
+    {
+        $banner = factory(Banner::class)->create(['campaign_id' => $campaignId]);
+        $bannerId = $banner->id;
+
+        return $bannerId;
+    }
+
+    public function testFailDeleteNotOwnedCampaign(): void
+    {
+        $this->actingAs(factory(User::class)->create(), 'api');
+
+        $user = factory(User::class)->create();
+        $campaignId = $this->createCampaignForUser($user);
+        $this->createBannerForCampaign($campaignId);
+
+        $response = $this->deleteJson(self::URI."/{$campaignId}");
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
     }
 }
