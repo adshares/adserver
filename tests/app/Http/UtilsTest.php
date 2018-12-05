49579af10c42b14d31c57b58c21d27c39ebcde1a
Przemysław Furtak <przemyslawf@gmail.com>
2018-12-05 11:30:45 +0100

    SRV-52 Create tracking cookie and send it to AdUser (#209)

---
+++ tests/app/Http/UtilsTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (c) 2018 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+declare(strict_types = 1);
+
+namespace Adshares\Adserver\Tests\Http;
+
+use Adshares\Adserver\Http\Utils;
+use PHPUnit\Framework\TestCase;
+
+class UtilsTest extends TestCase
+{
+    public function testIfCreateTrackingIdsAreTheSameWhenImpressionIdExists()
+    {
+        $impressionId = '1234567qweasd';
+        $secret = 'some-secret-123';
+
+        $trackingId1 = Utils::createTrackingId($secret, $impressionId);
+        $trackingId2 = Utils::createTrackingId($secret, $impressionId);
+
+        $this->assertEquals($trackingId1, $trackingId2);
+    }
+
+    public function testIfCreateTrackingIdsAreDifferentWhenNoImpressionId()
+    {
+        $secret = 'some-secret-123';
+
+        $trackingId1 = Utils::createTrackingId($secret);
+        $trackingId2 = Utils::createTrackingId($secret);
+
+        $this->assertNotEquals($trackingId1, $trackingId2);
+    }
+}
