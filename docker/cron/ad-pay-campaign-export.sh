6dfeeef26faf0bfc0495ef9032f914fb1f8616fa
PawelPodkalicki <ppodkali@gmail.com>
2018-12-13 13:33:23 +0100

    SRV-70 Update cron setup with new command signatures

--- docker/cron/ad-pay-campaign-export.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -e adpay_campaign_export_in_progress ]; then
-    touch adpay_campaign_export_in_progress
-
-    ./artisan adpay:campaign:export
-
-    rm -f adpay_campaign_export_in_progress
-fi
