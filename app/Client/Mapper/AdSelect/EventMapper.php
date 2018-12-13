f17bf7aa94d82386faa98caa76efa349267cda6d
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-12-13 11:30:55 +0100

    SRV-82 fixes after cr

--- app/Client/Mapper/AdSelect/EventMapper.php
+++ app/Client/Mapper/AdSelect/EventMapper.php
@@ -32,7 +32,7 @@ class EventMapper
         $keywords = self::normalizeKeywords($event['context']->site->keywords) ?? new stdClass();
         $mappedEvent = [
             'keywords' => $keywords,
-            'publisher_id' => $event['publisher_id'] ?? null,
+            'publisher_id' => $event['publisher_id'],
             'banner_id' => $event['banner_id'],
             'user_id' => $event['user_id'],
             'event_id' => $event['event_id'],
