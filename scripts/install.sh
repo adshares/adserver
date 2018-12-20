985befa1dffc26b7383ac6f84bec195f013f1558
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-12-20 20:13:25 +0100

    init

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,6 @@ mv * ${INSTALLATION_PATH}/
 mkdir -pm 777 ${INSTALLATION_PATH}/storage
 mkdir -pm 777 ${INSTALLATION_PATH}/storage/app/public/banners
 
-cd ${INSTALLATION_PATH}
-./bin/init.sh --stop
-./bin/init.sh --build --migrate --seed --start
+#cd ${INSTALLATION_PATH}
+#./bin/init.sh --stop
+#./bin/init.sh --build --migrate --seed --start
