a563c6c4271c1cfb89594360249a596ac51141a6
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-12-20 20:58:53 +0100

    .env

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,9 +7,10 @@ mkdir -p ${INSTALLATION_PATH}
 
 # Move directories
 mv * ${INSTALLATION_PATH}/
+mv .env* ${INSTALLATION_PATH}/
 
 mkdir -pm 777 ${INSTALLATION_PATH}/storage
 
-#cd ${INSTALLATION_PATH}
-#./bin/init.sh --stop
-#./bin/init.sh --build --migrate --seed --start
+cd ${INSTALLATION_PATH}
+#./bin/init.sh --build
+./bin/init.sh --migrate --seed --start
