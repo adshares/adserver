b614f4eb61aabf773c6130d4c568fb1fcb12943f
Paweł Zaremba <pawel.zaremba@adshares.pl>
2019-01-02 13:57:01 +0100

    Clear db on install

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,11 +17,21 @@ if [ ! -v TRAVIS ]; then
   ./artisan config:cache
 fi
 
+crontab -u ${INSTALLATION_USER} -r
+supervisorctl stop adselect
+supervisorctl stop adpay
+
 ./artisan migrate:fresh
 ./artisan db:seed
+mongo --eval 'db.dropDatabase()' adselect
+mongo --eval 'db.dropDatabase()' adpay
+
+supervisorctl start adselect
+supervisorctl start adpay
 
 #./artisan ops:targeting-options:update
-
 ./artisan ads:fetch-hosts
 
 crontab -u ${INSTALLATION_USER} ./docker/cron/crontab
+
+service php7.2-fpm restart
