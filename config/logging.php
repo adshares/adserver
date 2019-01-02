e347ee9f7289409c647cc991cc52f17dbd62cdb5
Paweł Zaremba <pawel.zaremba@adshares.pl>
2019-01-02 15:38:33 +0100

    logging

--- config/logging.php
+++ config/logging.php
@@ -69,9 +69,8 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => 'critical',
+            'level' => 'debug',
         ],
 
         'syslog' => [
