74acaa331a137a00e50d7ce02cb30aee3e78f289
Paweł Zaremba <pawel.zaremba@adshares.pl>
2019-01-22 15:45:58 +0100

    Log missing advertiser

--- app/Console/Commands/AdPayGetPayments.php
+++ app/Console/Commands/AdPayGetPayments.php
@@ -67,8 +67,13 @@ class AdPayGetPayments extends Command
         Log::info('Found '.count($unpaidEvents).' entries to update.');
 
         $ledgerUnpaidEvents = $unpaidEvents->groupBy(function (EventLog $entry) {
-            Log::debug(sprintf('%s', $entry->advertiser_id));
-            return User::fetchByUuid($entry->advertiser_id)->id;
+            $user = User::fetchByUuid($entry->advertiser_id);
+
+            if (!$user) {
+                Log::debug(sprintf('{"advertiser_id":"%s"}', $entry->advertiser_id));
+            }
+
+            return $user->id;
         })->map(function (Collection $collection, int $userId) use ($calculations) {
             $collection->each(function (EventLog $entry) use ($calculations) {
                 $calculation = $calculations->firstWhere('event_id', $entry->event_id);
