75aa2bb1471ca18fbc5d1ec57a1d72db6ba1210f
Paweł Zaremba <pawzar@users.noreply.github.com>
2019-01-23 13:41:12 +0100

    Remove mock & Remove  exception catching (#347)
    * null user id
    
    * Log missing advertiser
    
    * Log command
    
    * Typehint types
    
    * Remove mock.
    No exception catching

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -34,36 +34,6 @@ abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    /**
-     * @deprecated
-     */
-    public function mock(Request $request)
-    {
-        $pathInfo = str_replace(['/panel', '/app', '/api'], ['', '', ''], $request->getPathInfo());
-        $path = substr(str_replace('/', '_', $pathInfo), 1);
-        $method = strtolower($request->method());
-
-        $filePath = "mocks/mappings/{$path}_{$method}.json";
-
-        if (!is_file(base_path($filePath))) {
-            $filePath = "mocks/mappings/{$path}__{$method}.json";
-        }
-
-        try {
-            $json = file_get_contents(base_path($filePath));
-        } catch (\ErrorException $errorException) {
-            abort(404, $errorException->getMessage());
-        }
-
-        try {
-            $data = \GuzzleHttp\json_decode($json, true);
-
-            return self::json($data['response']['jsonBody']);
-        } catch (\Exception $errorException) {
-            return self::json([], 500, [$errorException->getMessage()]);
-        }
-    }
-
     protected static function json($data = [], $code = 200, $errors = false): JsonResponse
     {
         if (empty($errors)) {
