fa0ad5a6eafa6ba61b7edcfa2887e7df0ee304e0
Przemysław Furtak <przemyslawf@gmail.com>
2019-01-28 09:07:45 +0100

    SRV-198 Set always active banner status (#363)

--- app/Http/Controllers/Manager/CampaignsController.php
+++ app/Http/Controllers/Manager/CampaignsController.php
@@ -146,7 +146,7 @@ class CampaignsController extends Controller
 
             $bannerModel = new Banner();
             $bannerModel->name = $banner['name'];
-            $bannerModel->status = $banner['status'];
+            $bannerModel->status = Banner::STATUS_ACTIVE;
             $bannerModel->creative_width = $size[0];
             $bannerModel->creative_height = $size[1];
             $bannerModel->creative_type = Banner::type($banner['type']);
