06b4fbe20e92aacfe0b732f4c5a2cc725c673cfa
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-02-21 12:44:44 +0100

    SRV-212 Remove obsolete code

--- app/Dto/Response/Classifier/Item.php
+++ app/Dto/Response/Classifier/Item.php
@@ -96,13 +96,4 @@ class Item implements Arrayable
             'classified_site' => $this->classifiedSite,
         ];
     }
-
-//    public static function fromArray(array $data): self
-//    {
-//        return new self(
-//            $data['banner_id'],
-//            $data['url'],
-//
-//        );
-//    }
 }
