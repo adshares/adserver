a36efb1b55f6f33b25b25ea1150b3bdf5470874f
Paweł Zaremba <pawzar@users.noreply.github.com>
2019-02-28 13:25:58 +0100

    Prepare general balance checking (#438)

--- app/Console/Commands/AdPayGetPayments.php
+++ app/Console/Commands/AdPayGetPayments.php
@@ -48,7 +48,7 @@ class AdPayGetPayments extends Command
 
         DB::beginTransaction();
 
-        UserLedgerEntry::removeProcessingExpenditures();
+        UserLedgerEntry::removeProcessingExpenses();
 
         $ts = $this->option('timestamp');
         $timestamp = $ts === null ? now()->subHour((int)$this->option('sub'))->getTimestamp() : (int)$ts;
@@ -139,7 +139,7 @@ class AdPayGetPayments extends Command
                     $user->id,
                     -$totalEventValue,
                     UserLedgerEntry::STATUS_ACCEPTED,
-                    UserLedgerEntry::TYPE_AD_EXPENDITURE
+                    UserLedgerEntry::TYPE_AD_EXPENSE
                 );
 
                 $userLedgerEntry->save();
