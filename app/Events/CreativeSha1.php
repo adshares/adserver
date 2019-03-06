7f1dbcce5de3c0ebb798fedb4ed08f86342d94c6
Jacek Zemło <jacek@zel.pl>
2019-03-06 17:56:53 +0100

    inject csp and js to html banners

--- app/Events/CreativeSha1.php
+++ app/Events/CreativeSha1.php
@@ -20,6 +20,7 @@
 
 namespace Adshares\Adserver\Events;
 
+use Adshares\Adserver\Models\Banner;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -34,6 +35,49 @@ class CreativeSha1
      */
     public function __construct(\Adshares\Adserver\Models\Banner $model)
     {
+        if($model->creative_type == Banner::type(Banner::HTML_TYPE)) {
+            $model->creative_contents = $this->injectScriptAndCSP($model->creative_contents);
+        }
+
         $model->creative_sha1 = sha1($model->creative_contents);
+    }
+
+    private function injectScriptAndCSP($html) {
+        $jsPath = public_path('-/banner.js');
+        $jsCode = file_get_contents($jsPath);
+
+        $doc = new \DOMDocument();
+        $doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+
+        $xpath = new \DOMXPath($doc);
+        [$html] = $xpath->query('//html');
+        [$body] = $xpath->query('//body');
+        [$head] = $xpath->query('//head');
+
+        if(!$head) {
+            $head = $doc->createElement('head');
+            $html->insertBefore($head, $body);
+        }
+
+        $metas = $xpath->query('//head/meta');
+
+        $csp_tag = $doc->createElement('meta');
+        $csp_tag->setAttribute('http-equiv', "Content-Security-Policy");
+        $csp_tag->setAttribute('content', "default-src 'unsafe-inline' data: blob:");
+
+        if(!count($metas) || trim($doc->saveHTML($csp_tag)) != trim($doc->saveHTML($metas[0]))) {
+            $head->insertBefore($csp_tag, $head->firstChild);
+        }
+
+        $scripts = $xpath->query('//body/script');
+
+        $banner_script = $doc->createElement('script');
+        $banner_script->nodeValue = $jsCode;
+        if(!count($scripts) || $scripts[0]->nodeValue != $banner_script->nodeValue) {
+
+            $body->insertBefore($banner_script, $body->firstChild);
+        }
+
+        return $doc->saveHTML();
     }
 }
