420c266fb9c58fea77c34670eaf869dc553a42b5
Paweł Zaremba <pawel.zaremba@adshares.pl>
2019-03-06 12:04:29 +0100

    revert

--- tests/app/Console/AdsProcessTxTest.php
+++ tests/app/Console/AdsProcessTxTest.php
@@ -92,7 +92,7 @@ class AdsProcessTxTest extends TestCase
 
     public function testAdsProcessEventPayment(): void
     {
-        NetworkHost::registerHost('0001-00000000-9B6F', new Url('http://localhost'));
+        NetworkHost::registerHost('0001-00000000-9B6F', new Url('http://127.0.0.1'));
 
         $demandClient = new DummyDemandClient();
         $paymentDetails = $demandClient->fetchPaymentDetails('', '');
