3cf9937efb52901669c4530e119cff9a79ca4465
Jacek Zemło <jacek@zel.pl>
2019-03-06 21:00:10 +0100

    style

--- app/Events/CreativeSha1.php
+++ app/Events/CreativeSha1.php
@@ -35,14 +35,15 @@ class CreativeSha1
      */
     public function __construct(\Adshares\Adserver\Models\Banner $model)
     {
-        if($model->creative_type == Banner::type(Banner::HTML_TYPE)) {
+        if ($model->creative_type == Banner::type(Banner::HTML_TYPE)) {
             $model->creative_contents = $this->injectScriptAndCSP($model->creative_contents);
         }
 
         $model->creative_sha1 = sha1($model->creative_contents);
     }
 
-    private function injectScriptAndCSP($html) {
+    private function injectScriptAndCSP($html)
+    {
         $jsPath = public_path('-/banner.js');
         $jsCode = file_get_contents($jsPath);
 
@@ -54,7 +55,7 @@ class CreativeSha1
         [$body] = $xpath->query('//body');
         [$head] = $xpath->query('//head');
 
-        if(!$head) {
+        if (!$head) {
             $head = $doc->createElement('head');
             $html->insertBefore($head, $body);
         }
@@ -65,7 +66,7 @@ class CreativeSha1
         $csp_tag->setAttribute('http-equiv', "Content-Security-Policy");
         $csp_tag->setAttribute('content', "default-src 'unsafe-inline' data: blob:");
 
-        if(!count($metas) || trim($doc->saveHTML($csp_tag)) != trim($doc->saveHTML($metas[0]))) {
+        if (!count($metas) || trim($doc->saveHTML($csp_tag)) != trim($doc->saveHTML($metas[0]))) {
             $head->insertBefore($csp_tag, $head->firstChild);
         }
 
@@ -73,7 +74,7 @@ class CreativeSha1
 
         $banner_script = $doc->createElement('script');
         $banner_script->nodeValue = $jsCode;
-        if(!count($scripts) || $scripts[0]->nodeValue != $banner_script->nodeValue) {
+        if (!count($scripts) || $scripts[0]->nodeValue != $banner_script->nodeValue) {
 
             $body->insertBefore($banner_script, $body->firstChild);
         }
