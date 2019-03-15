13f4effce76d955ff2288f38f8b8cf5943f569e8
Paweł Zaremba <pawzar@gmail.com>
2019-03-15 15:23:43 +0100

    remove installer

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,24 +5,24 @@ source ${1:-$(dirname $(readlink -f "$0"))/bin}/_functions.sh
 [[ -z ${2:-""} ]] || cd $2
 [[ -z ${3:-".env"} ]] || set -a && source .env && set +a
 
-mkdir -pm 777 storage
-mkdir -pm 777 storage/app/public/banners
+function artisanCommand {
+    ./artisan --no-interaction "$@"
+}
 
 GIT_TAG=$(git tag -l --points-at HEAD | head -n 1)
 GIT_HASH="#"$(git rev-parse --short HEAD)
+
+mkdir -pm 777 storage
+mkdir -pm 777 storage/app/public/banners
 
 composer install --no-dev
 
 yarn install
 yarn run prod
 
-function artisanCommand {
-    ./artisan --verbose --no-interaction "$@"
-}
-
 artisanCommand key:generate
-artisanCommand config:cache
 artisanCommand storage:link
+artisanCommand config:cache
 
 if [[ ${DB_MIGRATE_FRESH:-0} -eq 1 ]]
 then
@@ -43,12 +43,22 @@ then
     artisanCommand db:seed
 fi
 
-if [[ ${TARGETING_UPDATE:-0} -eq 1 ]]
+if [[ ${UPDATE_TARGETING:-0} -eq 1 ]]
 then
     artisanCommand ops:targeting-options:update
 fi
 
-if [[ ${FILTERING_UPDATE:-0} -eq 1 ]]
+if [[ ${UPDATE_FILTERING:-0} -eq 1 ]]
 then
     artisanCommand ops:filtering-options:update
+fi
+
+if [[ ${UPDATE_NETWORK_HOSTS:-0} -eq 1 ]]
+then
+    artisanCommand ads:fetch-hosts --quiet
+fi
+
+if [[ ${BROADCAST_SERVER:-0} -eq 1 ]]
+then
+    artisanCommand ads:broadcast-host
 fi
