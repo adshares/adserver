e5697635337612f4f3f27e779b604038744e6a88
Paweł Zaremba <pawzar@gmail.com>
2019-03-28 18:36:29 +0100

    fix cron

--- deploy/crontablist.sh
+++ deploy/crontablist.sh
@@ -4,28 +4,28 @@ set -eu
 SERVICE_DIR=${SERVICE_DIR:-$(dirname $(dirname $(readlink -f $0)))}
 
 echo -n "*    *    * * * php ${SERVICE_DIR}/artisan ops:demand:inventory:import &> /dev/null && "
-echo "*    *    * * * php ${SERVICE_DIR}/artisan ops:adselect:inventory:export &> /dev/null"
+echo "php ${SERVICE_DIR}/artisan ops:adselect:inventory:export &> /dev/null"
 
 echo -n "*    *    * * * php ${SERVICE_DIR}/artisan ops:adpay:campaign:export &> /dev/null && "
-echo "*    *    * * * php ${SERVICE_DIR}/artisan ops:adpay:event:export &> /dev/null"
+echo "php ${SERVICE_DIR}/artisan ops:adpay:event:export &> /dev/null"
 
 echo -n "15   *    * * * php ${SERVICE_DIR}/artisan ops:adpay:payments:get &> /dev/null && "
-echo -n "15   *    * * * php ${SERVICE_DIR}/artisan ops:demand:payments:prepare &> /dev/null && "
-echo "15   *    * * * php ${SERVICE_DIR}/artisan ops:demand:payments:send &> /dev/null"
+echo -n "php ${SERVICE_DIR}/artisan ops:demand:payments:prepare &> /dev/null && "
+echo "php ${SERVICE_DIR}/artisan ops:demand:payments:send &> /dev/null"
 
 echo "59   *    * * * php ${SERVICE_DIR}/artisan ops:demand:payments:block &> /dev/null"
 
 echo -n "*/8  *    * * * php ${SERVICE_DIR}/artisan ads:get-tx-in &> /dev/null && "
-echo -n "*/8  *    * * * php ${SERVICE_DIR}/artisan ads:process-tx &> /dev/null && "
-echo -n "*/8  *    * * * php ${SERVICE_DIR}/artisan ops:supply:payments:send &> /dev/null && "
-echo "*/8  *    * * * php ${SERVICE_DIR}/artisan ops:adselect:payment:export &> /dev/null"
+echo -n "php ${SERVICE_DIR}/artisan ads:process-tx &> /dev/null && "
+echo -n "php ${SERVICE_DIR}/artisan ops:supply:payments:send &> /dev/null && "
+echo "php ${SERVICE_DIR}/artisan ops:adselect:payment:export &> /dev/null"
 
 echo "0    0    * * * php ${SERVICE_DIR}/artisan ops:license:fetch &> /dev/null"
 
 test ${SKIP_COLD_WALLET:-0} -eq 0 && \
 {
     echo -n "*/30 *    * * * php ${SERVICE_DIR}/artisan ops:wallet:transfer:cold &> /dev/null && "
-    echo "*/30 *    * * * php ${SERVICE_DIR}/artisan ops:wallet:transfer:check &> /dev/null"
+    echo "php ${SERVICE_DIR}/artisan ops:wallet:transfer:check &> /dev/null"
 }
 
 test ${SKIP_BROADCAST:-0} -eq 0 && \
