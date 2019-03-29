451f9a3f73bb420be799b8e1b78c8e7cada44d38
Paweł Zaremba <pawzar@gmail.com>
2019-03-29 14:32:17 +0100

    revert

--- src/Supply/Application/Dto/ImpressionContext.php
+++ src/Supply/Application/Dto/ImpressionContext.php
@@ -67,9 +67,10 @@ final class ImpressionContext
     public function adUserRequestBody(): array
     {
         return [
+            'site' => $this->site,
+            'device' => $this->device,
+            //BC with AdUser
             'headers' => $this->device['headers'] ?? [],
-            'tags' => [],
-            'url' => '',
         ];
     }
 
