ebaae8696de0123340148a1c16c02d9a14dec9d7
Przemysław Furtak <przemyslawf@gmail.com>
2019-04-08 15:03:29 +0200

    Hotfix publisher id nullable (#594)
    * PAN-296 Export only active campaigns in time meaning
    
    * Handle nullable publisher_id

---
+++ database/migrations/2019_04_08_125259_change_network_campaigns_publisher_id_null.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeNetworkCampaignsPublisherIdNull extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('network_campaigns', function (Blueprint $table) {
+            $table->binary('publisher_id', 16)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
