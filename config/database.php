a342b973759703c990d94d20d2ac6bfa684fd516
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-04-09 15:41:03 +0200

    SRV-290 Revert

--- config/database.php
+++ config/database.php
@@ -72,21 +72,6 @@ return [
             'engine' => null,
         ],
 
-        'mysql_testing' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
         'sqlite_testing' => [
             'driver'   => 'sqlite',
             'database' => ':memory:',
