a46d1a15c2f415b8a7f87f54e963ad34177d9427
Paweł Zaremba <pawzar@gmail.com>
2019-04-23 15:33:31 +0200

    remove log

--- app/Client/JsonRpcAdSelectClient.php
+++ app/Client/JsonRpcAdSelectClient.php
@@ -93,12 +93,12 @@ final class JsonRpcAdSelectClient implements AdSelect
 
         $items = $result->toArray();
 
-        Log::debug(sprintf(
-            '%s:%s %s',
-            __METHOD__,
-            __LINE__,
-            $procedure->toJson()
-        ));
+//        Log::debug(sprintf(
+//            '%s:%s %s',
+//            __METHOD__,
+//            __LINE__,
+//            $procedure->toJson()
+//        ));
 
         $bannerMap = $this->createRequestIdsToBannerMap($items);
         $bannerIds = $this->fixBannerOrdering($existingZones, $bannerMap);
