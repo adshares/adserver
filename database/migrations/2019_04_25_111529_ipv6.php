a0440837e5f822c6ae28457dd4adf910d9b134f3
Paweł Zaremba <pawzar@gmail.com>
2019-04-25 13:20:20 +0200

    Extend ip column size

---
+++ database/migrations/2019_04_25_111529_ipv6.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) 2018-2019 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+use Adshares\Adserver\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Ipv6 extends Migration
+{
+    public function up(): void
+    {
+        if (DB::isMysql()) {
+            DB::statement('ALTER TABLE event_logs MODIFY ip varbinary(16)');
+            DB::statement('ALTER TABLE network_event_logs MODIFY ip varbinary(16)');
+        } else {
+            Schema::table(
+                'event_logs',
+                function (Blueprint $table) {
+                    $table->binary('ip', 16)->change();
+                }
+            );
+            Schema::table(
+                'network_event_logs',
+                function (Blueprint $table) {
+                    $table->binary('ip', 16)->change();
+                }
+            );
+        }
+    }
+
+    public function down(): void
+    {
+    }
+}
