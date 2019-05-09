ed192d271243e4da67af06d07cb7d0e9920943d8
Paweł Zaremba <pawzar@gmail.com>
2019-05-09 14:31:20 +0200

    User List

--- app/Http/Controllers/Manager/UsersController.php
+++ app/Http/Controllers/Manager/UsersController.php
@@ -1,5 +1,25 @@
 <?php
 /**
+ * Copyright (c) 2018-2019 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+declare(strict_types = 1);
+/**
  * Copyright (c) 2018 Adshares sp. z o.o.
  *
  * This file is part of AdServer
@@ -22,17 +42,16 @@ namespace Adshares\Adserver\Http\Controllers\Manager;
 
 use Adshares\Adserver\Http\Controller;
 use Adshares\Adserver\Models\User;
-use Illuminate\Database\Eloquent\Collection;
 
 class UsersController extends Controller
 {
-    public function browse(): Collection
+    public function browse()
     {
-        return User::all();
+        return User::paginate();
     }
 
     public function count(): int
     {
-        return User::all()->count();
+        return User::count();
     }
 }
