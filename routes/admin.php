66adfaa2b695a0d2b4210ccbd05c9b096cd44bd3
Paweł Zaremba <pawzar@gmail.com>
2019-05-20 13:28:38 +0200

    Unify routing

--- routes/admin.php
+++ routes/admin.php
@@ -23,19 +23,22 @@ declare(strict_types = 1);
 use Adshares\Adserver\Http\Controllers\Manager\AdminController;
 use Adshares\Adserver\Http\Controllers\Manager\AuthController;
 use Adshares\Adserver\Http\Controllers\Manager\UsersController;
+use Adshares\Adserver\Http\Kernel;
 use Illuminate\Support\Facades\Route;
 
-Route::get('settings', [AdminController::class, 'listSettings']);
-Route::put('settings', [AdminController::class, 'updateSettings']);
+Route::middleware([Kernel::ADMIN_ACCESS, Kernel::JSON_API])->group(function () {
+    Route::get('settings', [AdminController::class, 'listSettings']);
+    Route::put('settings', [AdminController::class, 'updateSettings']);
 
-Route::get('license', [AdminController::class, 'getLicense']);
+    Route::get('license', [AdminController::class, 'getLicense']);
 
-Route::get('terms', [AdminController::class, 'getTerms']);
-Route::put('terms', [AdminController::class, 'putTerms']);
-Route::get('privacy', [AdminController::class, 'getPrivacyPolicy']);
-Route::put('privacy', [AdminController::class, 'putPrivacyPolicy']);
+    Route::get('terms', [AdminController::class, 'getTerms']);
+    Route::put('terms', [AdminController::class, 'putTerms']);
+    Route::get('privacy', [AdminController::class, 'getPrivacyPolicy']);
+    Route::put('privacy', [AdminController::class, 'putPrivacyPolicy']);
 
-Route::get('impersonation/{user}', [AuthController::class, 'impersonate']);
+    Route::get('impersonation/{user}', [AuthController::class, 'impersonate']);
 
-Route::get('users', [UsersController::class, 'browse'])
-    ->name('app.users.browse');
+    Route::get('users', [UsersController::class, 'browse'])
+        ->name('app.users.browse');
+});
