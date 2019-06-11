95cbbaa957ebb75271fa1bc19ee3f1de8f7ef9d9
Paweł Zaremba <pawzar@gmail.com>
2019-06-11 18:22:13 +0200

    mail encryption === `none`

--- config/mail.php
+++ config/mail.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Copyright (c) 2018 Adshares sp. z o.o.
+ * Copyright (c) 2018-2019 Adshares sp. z o.o.
  *
  * This file is part of AdServer
  *
- * AdServer is free software: you can redistribute it and/or modify it
+ * AdServer is free software: you can redistribute and/or modify it
  * under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
@@ -15,8 +15,13 @@
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AdServer.  If not, see <https://www.gnu.org/licenses/>
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
  */
+
+$mail_encryption = env('MAIL_ENCRYPTION', 'tls');
+if ($mail_encryption === 'none') {
+    $mail_encryption = '';
+}
 
 return [
 
@@ -89,7 +94,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => $mail_encryption,
 
     /*
     |--------------------------------------------------------------------------
