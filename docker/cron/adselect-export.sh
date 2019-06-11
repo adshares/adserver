aa85ccf3514e490c3bdc572e814ee6655cad990e
PawelPodkalicki <ppodkali@gmail.com>
2019-06-11 16:45:37 +0200

    fix file name

--- docker/cron/adselect-export.sh
+++ docker/cron/adselect-export.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -e /tmp/ops_inventory_in_progress ]; then
-    touch /tmp/ops_inventory_in_progress
+if [ ! -e /tmp/adselect_export_in_progress ]; then
+    touch /tmp/adselect_export_in_progress
 
     ./artisan ops:adselect:event:export
 
-    rm -f /tmp/ops_inventory_in_progress
+    rm -f /tmp/adselect_export_in_progress
 fi
