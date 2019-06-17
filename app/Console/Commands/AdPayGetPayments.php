db2b428561f97b8a2f17a4bfba1759b721b986ed
Paweł Zaremba <pawzar@users.noreply.github.com>
2019-06-17 15:04:21 +0200

    Chunked query (#751)
    * Chunked query
    
    * tests

--- app/Console/Commands/AdPayGetPayments.php
+++ app/Console/Commands/AdPayGetPayments.php
@@ -46,7 +46,7 @@ class AdPayGetPayments extends BaseCommand
 
     public const NORMAL = 'normal';
 
-    protected $signature = 'ops:adpay:payments:get {--t|timestamp=} {--s|sub=1} {--f|force}';
+    protected $signature = 'ops:adpay:payments:get {--t|timestamp=} {--s|sub=1} {--f|force} {--chunkSize=250}';
 
     public function handle(AdPay $adPay, ExchangeRateReader $exchangeRateReader): void
     {
@@ -98,9 +98,15 @@ class AdPayGetPayments extends BaseCommand
 
     private function getUnpaidEvents(Collection $eventIds): Collection
     {
-        return EventLog::whereIn('event_id', $eventIds)
-            ->whereNull('event_value_currency')
-            ->get();
+        return $eventIds
+            ->chunk((int)$this->option('chunkSize'))
+            ->flatMap(
+                static function (Collection $eventIds) {
+                    return EventLog::whereIn('event_id', $eventIds)
+                        ->whereNull('event_value_currency')
+                        ->get();
+                }
+            );
     }
 
     private function getCalculations(AdPay $adPay): Collection
