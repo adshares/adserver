f71536e46b8928eef2c9022a0dc064234f778e07
Paweł Zaremba <pawzar@users.noreply.github.com>
2019-06-24 17:18:46 +0200

    Synchronize indexes (#764)
    * Synchronize indexes
    
    * Synchronize indexes
    
    * Synchronize indexes

--- app/Models/NetworkEventLog.php
+++ app/Models/NetworkEventLog.php
@@ -73,6 +73,12 @@ class NetworkEventLog extends Model
 
     public const TYPE_CLICK = 'click';
 
+    public const INDEX_CREATED_AT = 'network_event_logs_created_at_index';
+
+    public const INDEX_UPDATED_AT = 'network_event_logs_updated_at_index';
+
+    public const INDEX_ADS_PAYMENT_ID = 'network_event_logs_ads_payment_id_index';
+
     use AccountAddress;
     use AutomateMutators;
     use BinHex;
