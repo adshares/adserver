67c68b3dabe2a8cf54c6d393cc30681ecba53643
PawelPodkalicki <ppodkali@gmail.com>
2019-07-29 15:14:27 +0200

    Add tests for BannerClassificationsCreateCommand

---
+++ tests/app/Console/Commands/BannerClassificationsCreateCommandTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) 2018-2019 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+declare(strict_types = 1);
+
+namespace Adshares\Adserver\Tests\Console\Commands;
+
+use Adshares\Adserver\Models\Banner;
+use Adshares\Adserver\Models\BannerClassification;
+use Adshares\Adserver\Models\Campaign;
+use Adshares\Adserver\Models\User;
+use Adshares\Adserver\Tests\Console\TestCase;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BannerClassificationsCreateCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private const CLASSIFIER_NAME = 'test_classifier';
+
+    public function testCreateBannerClassifications(): void
+    {
+        $bannerCount = 3;
+
+        $bannerCollection = new Collection();
+        for ($i = 0; $i < $bannerCount; $i++) {
+            $bannerCollection->add($this->insertBanner());
+        }
+
+        $this->artisan('ops:demand:classification:create', ['classifier' => self::CLASSIFIER_NAME])->assertExitCode(0);
+
+        $bannerClassificationCollection = BannerClassification::all();
+
+        $this->assertEquals($bannerCount, $bannerClassificationCollection->count());
+        $bannerClassificationCollection->each(
+            function ($item) {
+                /** @var BannerClassification $item */
+                $this->assertEquals(BannerClassification::STATUS_NEW, $item->status);
+                $this->assertEquals(self::CLASSIFIER_NAME, $item->classifier);
+                $this->assertNull($item->keywords);
+            }
+        );
+    }
+
+    private function insertBanner(): Banner
+    {
+        $user = factory(User::class)->create();
+        $campaign = factory(Campaign::class)->create(['user_id' => $user->id]);
+        $banner = factory(Banner::class)->create(['campaign_id' => $campaign->id]);
+        $banner->classifications()->save(BannerClassification::prepare(self::CLASSIFIER_NAME));
+
+        return $banner;
+    }
+}
