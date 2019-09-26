9a5deaa147a305208ea3e915b7ef25ec2cd9f473
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-09-26 16:12:19 +0200

    Clear events command

---
+++ app/Console/Commands/ClearEvents.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Copyright (c) 2018 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+declare(strict_types=1);
+
+namespace Adshares\Adserver\Console\Commands;
+
+use Adshares\Adserver\Facades\DB;
+use Adshares\Common\Infrastructure\Service\ExchangeRateReader;
+use Adshares\Demand\Application\Service\AdPay;
+use function sprintf;
+
+class ClearEvents extends BaseCommand
+{
+    public const DIRECT = 'direct';
+
+    public const NORMAL = 'normal';
+
+    protected $signature = 'ops:events:clear {--b|before=} {--p|period=P32D} {--c|chunkSize=1000}';
+
+    protected $description = 'Clear event and network event logs';
+
+
+    public function handle(AdPay $adPay, ExchangeRateReader $exchangeRateReader): void
+    {
+        if (!$this->lock()) {
+            $this->info(sprintf('Command %s already running', $this->signature));
+
+            return;
+        }
+        $this->info(sprintf('Start command %s', $this->signature));
+
+        $chunkSize = (int)$this->option('chunkSize');
+        $period = $this->option('period');
+        $before = $this->option('before');
+
+        try {
+            if ($before !== null) {
+                $dateTo = new \DateTime($before);
+            } else {
+                $dateTo = (new \DateTime())->sub(new \DateInterval($period));
+            }
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+            return;
+        }
+
+        $this->info(sprintf(
+                'Clearing events older than %s with chunk size %d',
+                $dateTo->format('c'),
+                $chunkSize)
+        );
+
+        $this->clearTable('event_logs', $dateTo, $chunkSize);
+        $this->clearTable('network_impressions', $dateTo, $chunkSize);
+        $this->clearTable('network_cases', $dateTo, $chunkSize);
+        $this->clearTable('network_case_clicks', $dateTo, $chunkSize);
+        $this->clearTable('network_case_payments', $dateTo, $chunkSize);
+    }
+
+    private function clearTable(string $table, \DateTime $dateTo, int $chunkSize): int
+    {
+        $this->getOutput()->write(sprintf('<info>Clearing %s', $table));
+
+        $deleted = 0;
+        $last = (int)DB::selectOne(
+            sprintf('SELECT MIN(id) AS value FROM %s WHERE created_at >= ?', $table),
+            [$dateTo]
+        )->value;
+
+        do {
+            $this->getOutput()->write('.');
+            $offset = (int)DB::selectOne(
+                sprintf('SELECT MIN(id) AS value FROM %s', $table)
+            )->value;
+            $count = DB::delete(
+                sprintf('DELETE FROM %s WHERE id BETWEEN ? AND ? AND id < ?', $table),
+                [
+                    $offset,
+                    $offset + $chunkSize - 1,
+                    $last
+                ]);
+            $deleted += $count;
+        } while ($count > 0);
+
+        $this->getOutput()->writeln('</info>');
+        $this->info(sprintf('Deleted %d rows from %s', $deleted, $table));
+
+        return $deleted;
+    }
+}
