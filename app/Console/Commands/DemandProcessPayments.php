99ae12dff82b8fe11c708c16043aab2cc2632427
PawelPodkalicki <ppodkali@gmail.com>
2019-11-07 16:05:27 +0100

    Add payment report id option

--- app/Console/Commands/DemandProcessPayments.php
+++ app/Console/Commands/DemandProcessPayments.php
@@ -21,17 +21,20 @@ declare(strict_types = 1);
 
 namespace Adshares\Adserver\Console\Commands;
 
+use Adshares\Adserver\Exceptions\ConsoleCommandException;
 use Adshares\Adserver\Models\Config;
 use Adshares\Adserver\Models\PaymentReport;
 use Adshares\Common\Exception\InvalidArgumentException;
 use DateTime;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Artisan;
 use Symfony\Component\Console\Exception\LogicException;
 
 class DemandProcessPayments extends BaseCommand
 {
     protected $signature = 'ops:demand:payments:process
-                            {--f|from= : Date from which reports will be processed}';
+                            {--f|from= : Date from which reports will be processed}
+                            {--ids= : Report ids to process}';
 
     protected $description = 'Fetches reports, sends payments and updates statistics';
 
@@ -47,7 +50,8 @@ class DemandProcessPayments extends BaseCommand
 
         PaymentReport::fillMissingReports();
         $lastAvailableTimestamp = $this->getLastAvailableTimestamp();
-        $reports = PaymentReport::fetchUndone($this->getReportDateFrom());
+        $isRecalculationNeeded = null !== $this->option('ids');
+        $reports = $this->fetchPaymentReports();
 
         /** @var PaymentReport $report */
         foreach ($reports as $report) {
@@ -57,11 +61,16 @@ class DemandProcessPayments extends BaseCommand
             }
 
             if ($report->isNew()) {
+                $getPaymentsParameters = ['--timestamp' => $timestamp];
+                if ($isRecalculationNeeded) {
+                    $getPaymentsParameters['--recalculate'] = 1;
+                }
+
                 try {
                     $status =
                         Artisan::call(
                             AdPayGetPayments::COMMAND_SIGNATURE,
-                            ['--timestamp' => $timestamp],
+                            $getPaymentsParameters,
                             $this->getOutput()
                         );
                 } catch (LogicException $logicException) {
@@ -81,12 +90,15 @@ class DemandProcessPayments extends BaseCommand
             }
 
             if ($report->isUpdated()) {
-                $parameters = [
-                    '--from' => (new DateTime('@'.$timestamp))->format(DateTime::ATOM),
-                    '--to' => (new DateTime('@'.($timestamp + 3599)))->format(DateTime::ATOM),
-                ];
                 try {
-                    Artisan::call(DemandPreparePayments::COMMAND_SIGNATURE, $parameters, $this->getOutput());
+                    Artisan::call(
+                        DemandPreparePayments::COMMAND_SIGNATURE,
+                        [
+                            '--from' => (new DateTime('@'.$timestamp))->format(DateTime::ATOM),
+                            '--to' => (new DateTime('@'.($timestamp + 3599)))->format(DateTime::ATOM),
+                        ],
+                        $this->getOutput()
+                    );
                 } catch (LogicException $logicException) {
                     $this->warn(sprintf('Command %s is locked', DemandPreparePayments::COMMAND_SIGNATURE));
 
@@ -126,6 +138,25 @@ class DemandProcessPayments extends BaseCommand
         }
 
         $this->info('End command '.$this->getName());
+    }
+
+    private function fetchPaymentReports(): Collection
+    {
+        if (null !== ($ids = $this->option('ids'))) {
+            $reports = PaymentReport::fetchByIds(explode(',', $ids));
+
+            if ($reports->isEmpty()) {
+                throw new ConsoleCommandException(sprintf('Payment reports "%s" not found', $ids));
+            }
+
+            return $reports->each(
+                function (PaymentReport $report) {
+                    $report->setNew();
+                }
+            );
+        }
+
+        return PaymentReport::fetchUndone($this->getReportDateFrom());
     }
 
     private function getReportDateFrom(): DateTime
