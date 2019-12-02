82da52a146a52d20a0171a417f9e799a4d24f429
PawelPodkalicki <ppodkali@gmail.com>
2019-12-02 12:02:13 +0100

    Update DummyAdSelectClient

--- tests/mock/Client/DummyAdSelectClient.php
+++ tests/mock/Client/DummyAdSelectClient.php
@@ -28,15 +28,16 @@ use Adshares\Adserver\Models\NetworkBanner;
 use Adshares\Adserver\Models\NetworkCampaign;
 use Adshares\Adserver\Models\Zone;
 use Adshares\Adserver\Utilities\AdsUtils;
+use Adshares\Common\Domain\ValueObject\SecureUrl;
 use Adshares\Supply\Application\Dto\FoundBanners;
 use Adshares\Supply\Application\Dto\ImpressionContext;
 use Adshares\Supply\Application\Service\AdSelect;
-use Adshares\Supply\Domain\Model\Campaign;
 use Adshares\Supply\Domain\Model\CampaignCollection;
 use Adshares\Supply\Domain\ValueObject\Status;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
 use InvalidArgumentException;
+use function bin2hex;
 use function hex2bin;
 
 final class DummyAdSelectClient implements AdSelect
@@ -51,6 +52,7 @@ final class DummyAdSelectClient implements AdSelect
     private function getBestBanners(array $zones): array
     {
         $bannerIds = [];
+        $zoneData = [];
         foreach ($zones as $zoneInfo) {
             $zone = Zone::where('uuid', hex2bin($zoneInfo['zone']))->first();
             if (!$zone) {
@@ -60,7 +62,12 @@ final class DummyAdSelectClient implements AdSelect
 
             try {
                 $queryBuilder = $this->queryBuilder($zone);
-                $bannerIds[] = bin2hex($queryBuilder->get(['network_banners.uuid'])->random()->uuid);
+                $bannerId = bin2hex($queryBuilder->get(['network_banners.uuid'])->random()->uuid);
+                $bannerIds[] = $bannerId;
+                $zoneData[$bannerId] = [
+                    'publisher_id'  => $zone->site->user->uuid,
+                    'zone_id'       => $zone->uuid,
+                ];
             } catch (InvalidArgumentException $e) {
                 $bannerIds[] = '';
             }
@@ -68,31 +75,41 @@ final class DummyAdSelectClient implements AdSelect
 
         $banners = [];
         foreach ($bannerIds as $bannerId) {
-            $banner = $bannerId ? NetworkBanner::where('uuid', hex2bin($bannerId))->first() : NetworkBanner::first();
+            $banner = $bannerId ? NetworkBanner::where('uuid', hex2bin($bannerId))->first() : null;
 
             if (empty($banner)) {
                 $banners[] = null;
             } else {
                 $campaign = NetworkCampaign::find($banner->network_campaign_id);
                 $banners[] = [
+                    'id' => $bannerId,
+                    'publisher_id' => $zoneData[$bannerId]['publisher_id'],
+                    'zone_id' => $zoneData[$bannerId]['zone_id'],
                     'pay_from' => $campaign->source_address,
                     'pay_to' => AdsUtils::normalizeAddress(config('app.adshares_address')),
+                    'type' => $banner->type,
+                    'size' => $banner->size,
                     'serve_url' => $banner->serve_url,
                     'creative_sha1' => $banner->checksum,
-                    'click_url' => route(
-                        'log-network-click',
-                        [
-                            'id' => $banner->uuid,
-                            'r' => Utils::urlSafeBase64Encode($banner->click_url),
-                        ]
+                    'click_url' => SecureUrl::change(
+                        route(
+                            'log-network-click',
+                            [
+                                'id' => $banner->uuid,
+                                'r'  => Utils::urlSafeBase64Encode($banner->click_url),
+                            ]
+                        )
                     ),
-                    'view_url' => route(
-                        'log-network-view',
-                        [
-                            'id' => $banner->uuid,
-                            'r' => Utils::urlSafeBase64Encode($banner->view_url),
-                        ]
+                    'view_url' => SecureUrl::change(
+                        route(
+                            'log-network-view',
+                            [
+                                'id' => $banner->uuid,
+                                'r'  => Utils::urlSafeBase64Encode($banner->view_url),
+                            ]
+                        )
                     ),
+                    'rpm' => 0.5,
                 ];
             }
         }
