e3ed24d32a1442ee9c92e55714a2457c1b60dbe5
smartsir796 <smartsir796@adshares.net>
2021-03-04 18:11:57 +0100

    direct iframe click redirects

--- resources/js/supply/find/find.js
+++ resources/js/supply/find/find.js
@@ -117,7 +117,10 @@ var prepareElement = function (context, banner, element, contextParam) {
                 var act = document.activeElement;
                 if(act && act == element){
                     clearInterval(monitor);
-                    addTrackingImage(addUrlParam(context.click_url, 'logonly', 1));
+                    if (!winOpen(url, '_blank')) {
+                        topwin.location.href = url;
+                    }
+                    //addTrackingImage(addUrlParam(context.click_url, 'logonly', 1));
                     element.blur();
                 }
             }, 100);
