d01f908b6f34050a40b1f72642e386526ae7b0cf
Paweł Podkalicki <38037054+PawelPodkalicki@users.noreply.github.com>
2022-07-04 14:24:18 +0200

    Add JWT authentication for other services (#1176)
    * Add JWT
    
    * Update changelog
    
    * Add token refresh
    
    * update lock
    
    * introduce AppMode
    
    * rename field to mode
    
    * Fox code style

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [1.18.4] - 2022-06-30
 ### Added
 - Command listing administrators
+- JWT authentication for other services
 ### Fixed
 - Do not log entered password
 - Guest requests from DCL
---
+++ src/Config/AppMode.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Copyright (c) 2018-2022 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Config;
+
+use Adshares\Common\Exception\RuntimeException;
+
+final class AppMode
+{
+    public const INITIALIZATION = 'initialization';
+    public const MAINTENANCE = 'maintenance';
+    public const OPERATIONAL = 'operational';
+
+    public function __construct()
+    {
+        throw new RuntimeException();
+    }
+
+    private static function cases(): array
+    {
+        return [
+            self::INITIALIZATION,
+            self::MAINTENANCE,
+            self::OPERATIONAL,
+        ];
+    }
+
+    public static function validate(string $value): void
+    {
+        if (!in_array($value, self::cases())) {
+            throw new RuntimeException(sprintf('Given value %s is not correct.', $value));
+        }
+    }
+
+    public static function getAppMode(): string
+    {
+        return 1 === config('app.setup') ? self::INITIALIZATION : self::OPERATIONAL;
+    }
+}
