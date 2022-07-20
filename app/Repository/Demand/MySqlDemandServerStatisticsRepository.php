4751a0ffd887ce774b5a81b11a35b9bb5cb81ac8
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2022-07-20 11:39:59 +0200

    Banners stats

--- app/Repository/Demand/MySqlDemandServerStatisticsRepository.php
+++ app/Repository/Demand/MySqlDemandServerStatisticsRepository.php
@@ -100,6 +100,8 @@ SQL;
     private const QUERY_BANNERS_SIZES = <<<SQL
 SELECT
   creative_size AS size,
+  c.medium,
+  c.vendor,
   IFNULL(SUM(e.views), 0)                          AS impressions,
   COUNT(*)                                         AS number
 FROM banners b
@@ -114,7 +116,7 @@ FROM banners b
   ) e ON e.banner_id = b.uuid
 WHERE b.status = 2
   AND b.deleted_at IS NULL
-GROUP BY 1;
+GROUP BY 1, 2, 3;
 SQL;
 
     public function fetchStatistics(): array
