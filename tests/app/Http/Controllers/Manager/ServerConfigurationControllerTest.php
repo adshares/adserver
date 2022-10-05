58b1a3676da5509dfc5f4bf04d2848860c322c2f
PawełPodkalicki <ppodkali@gmail.com>
2022-10-05 12:41:42 +0200

    Fix test - limit 0 is invalid

--- tests/app/Http/Controllers/Manager/ServerConfigurationControllerTest.php
+++ tests/app/Http/Controllers/Manager/ServerConfigurationControllerTest.php
@@ -199,8 +199,7 @@ final class ServerConfigurationControllerTest extends TestCase
         return [
             'ADSHARES_ADDRESS' => [Config::ADSHARES_ADDRESS, '0001-00000003-AB0C'],
             'HOURS_UNTIL_INACTIVE_HOST_REMOVAL' => [Config::HOURS_UNTIL_INACTIVE_HOST_REMOVAL, '168'],
-            'INVENTORY_FAILED_CONNECTION_LIMIT #1' => [Config::INVENTORY_FAILED_CONNECTION_LIMIT, '0'],
-            'INVENTORY_FAILED_CONNECTION_LIMIT #2' => [Config::INVENTORY_FAILED_CONNECTION_LIMIT, '8'],
+            'INVENTORY_FAILED_CONNECTION_LIMIT' => [Config::INVENTORY_FAILED_CONNECTION_LIMIT, '8'],
             'REFERRAL_REFUND_COMMISSION' => [Config::REFERRAL_REFUND_COMMISSION, '0'],
             'REFERRAL_REFUND_ENABLED' => [Config::REFERRAL_REFUND_ENABLED, '1'],
             'SUPPORT_EMAIL' => [Config::SUPPORT_EMAIL, 'sup@example.com'],
@@ -323,7 +322,7 @@ final class ServerConfigurationControllerTest extends TestCase
             'invalid user role (empty)' => [[Config::DEFAULT_USER_ROLES => '']],
             'invalid user role (invalid)' => [[Config::DEFAULT_USER_ROLES => 'invalid']],
             'invalid rejected domains' => [['rejected-domains' => 'a,b']],
-            'invalid inventory failed connection limit' => [[Config::INVENTORY_FAILED_CONNECTION_LIMIT => '-1']],
+            'invalid inventory failed connection limit' => [[Config::INVENTORY_FAILED_CONNECTION_LIMIT => '0']],
             'invalid inactive host removal period' => [[Config::HOURS_UNTIL_INACTIVE_HOST_REMOVAL => '0']],
         ];
     }
