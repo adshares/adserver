2f2bbf7c88e6f6fc649d1b25684c12ce5b56742e
Paweł Podkalicki <38037054+PawelPodkalicki@users.noreply.github.com>
2023-02-13 16:36:54 +0100

    Do not accept cryptovoxels builder (#1333)

--- app/Utilities/SiteUtils.php
+++ app/Utilities/SiteUtils.php
@@ -60,9 +60,7 @@ class SiteUtils
     public static function isValidCryptovoxelsUrl(string $url): bool
     {
         return 1 === preg_match(
-            '~^https://scene-'
-            . '(\d+|[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12})'
-            . '.cryptovoxels.com$~i',
+            '~^https://scene-\d+.cryptovoxels.com$~i',
             $url,
         );
     }
