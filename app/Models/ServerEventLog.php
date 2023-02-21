105712e38117343e84d8e799d4d125f6cdfed640
PawełPodkalicki <ppodkali@gmail.com>
2023-02-21 11:43:30 +0100

    Fix code style

--- app/Models/ServerEventLog.php
+++ app/Models/ServerEventLog.php
@@ -1,9 +1,29 @@
 <?php
 
+/**
+ * Copyright (c) 2018-2023 Adshares sp. z o.o.
+ *
+ * This file is part of AdServer
+ *
+ * AdServer is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
+
 namespace Adshares\Adserver\Models;
 
 use Adshares\Adserver\ViewModel\ServerEventType;
 use DateTimeInterface;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
@@ -13,6 +33,7 @@ use Illuminate\Support\Carbon;
  * @property Carbon created_at
  * @property ServerEventType type
  * @property array properties
+ * @mixin Builder
  */
 class ServerEventLog extends Model
 {
