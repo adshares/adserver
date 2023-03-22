6f3de1b1da2b40120f328a2fefaa7d1af8f96492
Hashem Nik <hashemnik@gmail.com>
2023-03-22 18:44:15 +0330

    bugFix for exclude sites (#37)
    Co-authored-by: Hashemnik <hashem@swashapp.io>

--- app/Services/Supply/SiteFilteringUpdater.php
+++ app/Services/Supply/SiteFilteringUpdater.php
@@ -76,7 +76,7 @@ class SiteFilteringUpdater
         if(config('app.reviewer_user_id')){
             $requireKeywords = $this->getClassificationForRejectedBannersForAllSites(intval(config('app.reviewer_user_id')));
             foreach ($requireKeywords as $requireKeyword) {
-                $siteRequires[self::INTERNAL_CLASSIFIER_NAMESPACE][] = $requireKeyword->keyword();
+                $siteExcludes[self::INTERNAL_CLASSIFIER_NAMESPACE][] = $requireKeyword->keyword();
             }
         }
 
